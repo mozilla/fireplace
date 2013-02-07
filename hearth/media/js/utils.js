f8c9a4e1e0ef0a711db5260e82b43be235b23e3e
Chris Van <cvan@mozilla.com>
2013-02-06 22:36:36 -0800

    add @dspasovski's clear X search hotness

--- hearth/media/js/utils.js
+++ hearth/media/js/utils.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore'], function($, _) {
+define(['jquery', 'underscore', 'z'], function($, _, z) {
     function getVars(qs, excl_undefined) {
         if (typeof qs === 'undefined') {
             qs = location.search;
@@ -37,24 +37,7 @@ define(['jquery', 'underscore'], function($, _) {
                 .replace(/'/g, '&#39;').replace(/"/g, '&#34;');
     };
 
-
-    // Sexy setTimeout. wait(1000).then(doSomething);
-    function wait(ms) {
-        var $d = $.Deferred();
-        setTimeout(function() {
-            $d.resolve();
-        }, ms);
-        return $d.promise();
-    }
-
-
     _.extend(String.prototype, {
-        startsWith: function(str) {
-            return this.slice(0, str.length) == str;
-        },
-        endsWith: function(str) {
-            return this.slice(-str.length) == str;
-        },
         trim: function(str) {
             // Trim leading and trailing whitespace (like lstrip + rstrip).
             return this.replace(/^\s*/, '').replace(/\s*$/, '');
@@ -65,22 +48,11 @@ define(['jquery', 'underscore'], function($, _) {
         }
     });
 
-
-    // .exists()
-    // This returns true if length > 0.
-    $.fn.exists = function(callback, args) {
-        var len = $(this).length;
-        if (len && callback) {
-            callback.apply(null, args);
-        }
-        return !!len;
-    };
-
     function makeOrGetOverlay(id) {
         var el = document.getElementById(id);
         if (!el) {
             el = $('<div class="overlay" id="' + id +'">');
-            $('body').append(el);
+            z.body.append(el);
         }
         return $(el);
     }
@@ -92,42 +64,6 @@ define(['jquery', 'underscore'], function($, _) {
         }
         // Otherwise, return undefined.
     }
-
-    // Initializes character counters for textareas.
-    function initCharCount() {
-        var countChars = function(el, cc) {
-            var $el = $(el),
-                val = $el.val(),
-                max = parseInt(cc.attr('data-maxlength'), 10),
-                left = max - val.length,
-                cc_parent = cc.parent();
-            // L10n: {0} is the number of characters left.
-            cc.html(format(ngettext('<b>{0}</b> character left.',
-                                    '<b>{0}</b> characters left.', left), [left]))
-              .toggleClass('error', left < 0);
-            if(left >= 0 && cc_parent.hasClass('error')) {
-                cc_parent.removeClass('error');
-            }
-        };
-        $('.char-count').each(function() {
-            var $cc = $(this),
-                $form = $(this).closest('form'),
-                $el;
-            if ($cc.attr('data-for-startswith') !== undefined) {
-                $el = $('textarea[id^="' + $cc.attr('data-for-startswith') + '"]:visible', $form);
-            } else {
-                $el = $('textarea#' + $cc.attr('data-for'), $form);
-            }
-            $el.bind('keyup blur', _.throttle(function() {
-                countChars(this, $cc);
-            }, 250)).trigger('blur');
-        });
-    }
-
-
-    $('html').ajaxSuccess(function(event, xhr, ajaxSettings) {
-        z.win.trigger('resize'); // Redraw what needs to be redrawn.
-    });
 
     return {
         '_pd': _pd,
