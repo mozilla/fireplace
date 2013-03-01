a7621f5a169065bf556a481c8eb2ec05785b9959
Matt Basta <me@mattbasta.com>
2013-03-01 15:08:42 -0800

    Update nunjucks for custom tags

--- hearth/media/js/settings_local.js
+++ hearth/media/js/settings_local.js
@@ -1,4 +1,4 @@
-define('settings', ['settings_base'], function(settings_base) {
+define('settings', ['settings_base', 'underscore'], function(settings_base, _) {
     // Override settings here!
     settings_base = _.defaults(settings_base, {});
     return settings_base;
