aced946f89a8972c06cc019abe145361e18eb8fb
Chris Van <cvan@mozilla.com>
2013-03-21 13:42:23 -0700

    fix order of _.defaults arguments in settings_local.js

--- hearth/media/js/settings_local.js
+++ hearth/media/js/settings_local.js
@@ -1,5 +1,5 @@
 define('settings', ['settings_base', 'underscore'], function(settings_base, _) {
     // Override settings here!
-    settings_base = _.defaults(settings_base, {});
+    settings_base = _.defaults({}, settings_base);
     return settings_base;
 });
