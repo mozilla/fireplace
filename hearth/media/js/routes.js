912b060d9facc5664ac9059964eb6b413467a744
Matt Basta <me@mattbasta.com>
2013-03-20 20:42:08 -0700

    Get our packaged app builds working

--- hearth/media/js/routes.js
+++ hearth/media/js/routes.js
@@ -1,5 +1,5 @@
 var routes = [
-    {pattern: '^/$', view_name: 'homepage'},
+    {pattern: '^/(index.html)?$', view_name: 'homepage'},
     {pattern: '^/app/([^/<>"\']+)/ratings$', view_name: 'app/ratings'},
     {pattern: '^/app/([^/<>"\']+)/abuse$', view_name: 'app/abuse'},
     {pattern: '^/app/([^/<>"\']+)/privacy$', view_name: 'app/privacy'},
