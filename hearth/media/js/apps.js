b3a2cd33d7ecb45adc487cba3ab0e64e58e67383
Matt Basta <me@mattbasta.com>
2013-03-28 16:26:24 -0700

    apps.js tests (bug 851575)

--- hearth/media/js/apps.js
+++ hearth/media/js/apps.js
@@ -1,12 +1,13 @@
 /*
     Provides the apps module, a wrapper around navigator.mozApps
 */
-define('apps', ['capabilities', 'z'], function() {
+define('apps', ['jquery', 'underscore'], function($, _) {
     'use strict';
 
     /*
 
     apps.install(manifest_url, options)
+    apps.installPackage(manifest_url, options)
 
     It's just like navigator.apps.install with the following enhancements:
     - If navigator.apps.install doesn't exist, an error is displayed
@@ -14,7 +15,7 @@ define('apps', ['capabilities', 'z'], function() {
 
     This requires at least one apps-error-msg div to be present.
 
-    See also: https://developer.mozilla.org/en-US/docs/Apps/Apps_JavaScript_API/navigator.mozApps.install
+    See also: https://developer.mozilla.org/docs/DOM/Apps.install
 
     The recognized option attributes are as follows:
 
@@ -24,45 +25,34 @@ define('apps', ['capabilities', 'z'], function() {
         Optional callback for when app installation was successful
     error
         Optional callback for when app installation resulted in error
-    domContext
-        Something other than document, useful for testing
     navigator
         Something other than the global navigator, useful for testing
 
     */
     function install(product, opt) {
-        opt = $.extend({'domContext': document,
-                        'navigator': navigator,
-                        'data': {}},
-                       opt || {});
+        opt = opt || {};
+        _.defaults(opt, {'navigator': navigator,
+                         'data': {}});
         opt.data.categories = product.categories;
-        var errSummary,
-            manifest_url = product.manifest_url,
-            $def = $.Deferred();
+        var manifest_url = product.manifest_url;
+        var $def = $.Deferred();
+        var mozApps = opt.navigator.mozApps;
 
-        /* Try and install the app. */
-        if (manifest_url && opt.navigator.mozApps && opt.navigator.mozApps.install) {
-            /* TODO: combine the following check with the above. But don't stop
-             * clients without installPackage from using apps for now.
-             */
-            if (product.is_packaged && !opt.navigator.mozApps.installPackage && !manifest_url) {
-                $def.reject();
-            }
-            var installRequest;
-            if (product.is_packaged) {
-                installRequest = opt.navigator.mozApps.installPackage(manifest_url, opt.data);
-            } else {
-                installRequest = opt.navigator.mozApps.install(manifest_url, opt.data);
-            }
+        /* Try to install the app. */
+        if (manifest_url && mozApps &&
+            (product.is_packaged ? mozApps.installPackage: mozApps.install)) {
+
+            var installRequest = (
+                mozApps[product.is_packaged ? 'installPackage' : 'install'](manifest_url, opt.data));
             installRequest.onsuccess = function() {
                 var status;
                 var isInstalled = setInterval(function() {
-                    // "status" is now deprecated in favor of "installState" (bug 796016).
-                    status = installRequest.result.status || installRequest.result.installState;
+                    status = installRequest.result.installState;
                     if (status == 'installed') {
                         clearInterval(isInstalled);
                         $def.resolve(installRequest.result, product);
                     }
+                    // TODO: What happens if there's an installation failure? Does this never end?
                 }, 100);
             };
             installRequest.onerror = function() {
---
+++ hearth/tests/apps.js
@@ -0,0 +1,146 @@
+(function() {
+var a = require('assert');
+var assert = a.assert;
+var eq_ = a.eq_;
+var contains = a.contains;
+var mock = a.mock;
+
+function MockJQuery() {
+    this.Deferred = $.Deferred;
+}
+
+function MockNavigator(no_package) {
+    var def = this.def = $.Deferred();
+    function installer(url, data) {
+        var robj = {
+            result: null
+        };
+        def.done(function(result) {
+            robj.result = result;
+            if (robj.onsuccess) {
+                robj.onsuccess.apply(this);
+            }
+        }).fail(function(result) {
+            robj.result = result;
+            this.error = {name: 'Test Error'};
+            if (robj.onerror) {
+                robj.onerror.apply(this);
+            }
+        });
+        return robj;
+    }
+    this.mozApps = {
+        install: installer,
+        installPackage: installer
+    };
+    if (no_package) {
+        this.mozApps.installPackage = undefined;
+    }
+}
+
+test('apps.install', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator();
+            var product = {
+                manifest_url: 'foo/bar',
+                is_packaged: false
+            };
+            apps.install(product, {navigator: nav}).done(done);
+
+            nav.def.resolve({installState: 'installed'});
+        }
+    );
+});
+
+test('apps.install delay', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator();
+            var product = {
+                manifest_url: 'foo/bar',
+                is_packaged: false
+            };
+            apps.install(product, {navigator: nav}).done(done);
+
+            var result = {installState: 'pending'};
+            nav.def.resolve(result);
+            setTimeout(function() {
+                result.installState = 'installed';
+            }, 400);
+        }
+    );
+});
+
+test('apps.install packaged', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator();
+            var product = {
+                manifest_url: 'foo/bar',
+                is_packaged: true
+            };
+            apps.install(product, {navigator: nav}).done(done);
+            nav.def.resolve({installState: 'installed'});
+        }
+    );
+});
+
+test('apps.install unable', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator();
+            var product = {
+                manifest_url: null,
+                is_packaged: false
+            };
+            apps.install(product, {navigator: nav}).done(fail).fail(done);
+            // This shouldn't ever be used, but if it is, it'll trigger a failure.
+            nav.def.resolve({installState: 'installed'});
+        }
+    );
+});
+
+test('apps.install unable packaged', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator(true);
+            var product = {
+                manifest_url: 'foo/bar',
+                is_packaged: true
+            };
+            apps.install(product, {navigator: nav}).done(fail).fail(done);
+            // This shouldn't ever be used, but if it is, it'll trigger a failure.
+            nav.def.resolve({installState: 'installed'});
+        }
+    );
+});
+
+test('apps.install fail', function(done, fail) {
+    mock(
+        'apps',
+        {jquery: new MockJQuery()},
+        function(apps) {
+            var nav = new MockNavigator();
+            var product = {
+                manifest_url: 'foo/bar',
+                is_packaged: false
+            };
+            apps.install(product, {navigator: nav}).fail(done).done(fail);
+
+            nav.def.reject({});
+        }
+    );
+});
+
+})();
