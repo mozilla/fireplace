bbaf9cfd18d0462a4afc376b4c4903dd028f9f7d
Matt Claypotch <thepotch@gmail.com>
2013-03-29 15:18:16 -0700

    allow debug toggling

--- hearth/media/js/views/debug.js
+++ hearth/media/js/views/debug.js
@@ -1,7 +1,18 @@
 define(
-    ['capabilities'],
-    function(capabilities) {
+    ['capabilities', 'notification', 'z'],
+    function(capabilities, 'z') {
     'use strict';
+
+    z.doc.on('click', '#toggle-debug', function() {
+        var debugEnabled = localStorage.getItem('debug-enabled');
+        if (debugEnabled) {
+            notification.notification({message: 'debug mode disabled', timeout: 1000});
+            localStorage.setItem('debug-enabled', false);
+        } else {
+            notification.notification({message: 'debug mode disabled', timeout: 1000});
+            localStorage.setItem('debug-enabled', true);
+        }
+    });
 
     return function debug_view(builder, args) {
         console.log(capabilities);
