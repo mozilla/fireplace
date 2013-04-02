a2d34b1918d3fefd311acc7deaa1df937d59d09f
Matt Basta <me@mattbasta.com>
2013-04-01 18:28:36 -0700

    Start using chimney in Inferno builds

--- hearth/media/js/settings_inferno.js
+++ hearth/media/js/settings_inferno.js
@@ -1,7 +1,7 @@
 define('settings', ['settings_base', 'underscore'], function(settings_base, _) {
     // Override settings here!
     settings_base = _.defaults({
-        api_url: 'http://flue.paas.allizom.org',
+        api_url: 'http://chimney.paas.allizom.org',
         body_classes: 'nightly'
     }, settings_base);
     return settings_base;
