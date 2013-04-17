fd8cb09af71d279607ed3f3248b3d9259a927392
Matt Basta <me@mattbasta.com>
2013-04-17 13:37:39 -0700

    Add ETags to Inferno so Fireplace Nightly updates on its own <3

--- inferno/app.js
+++ inferno/app.js
@@ -1,4 +1,5 @@
 var cp = require('child_process');
+var crypto = require('crypto');
 var fs = require('fs');
 var http = require('https');
 
@@ -12,16 +13,49 @@ app.get('/', function(req, res){
     res.send(fs.readFileSync('templates/index.html') + '');
 });
 
-app.get('/minifest', function(req, res){
+function getMinifest(type, res) {
     var manifest = JSON.parse(fs.readFileSync('build/fireplace-master/hearth/manifest.webapp'));
-    manifest['package_path'] = 'http://inferno.paas.allizom.org/package.zip';
+    manifest['package_path'] = 'http://inferno.paas.allizom.org/package.zip?type=' + type;
+
     res.set('Content-Type', 'application/x-web-app-manifest+json');
+    res.set('ETag', '"' + getETag(type, null, 'build/package.zip') + '"');
     res.send(JSON.stringify(manifest));
+}
+
+app.get('/minifest', function(req, res){
+    getMinifest('daily', res);
+});
+app.get('/minifest/:type', function(req, res){
+    getMinifest(req.params.type, res);
 });
 
+function getETag(type, data, path) {
+    var output = '';
+    var now = new Date();
+    switch (type) {
+        case 'latest':
+            var hash = crypto.createHash('md5');
+            if (!data) {
+                data = fs.readFileSync(path);
+            }
+            hash.update(data);
+            return hash.digest('hex');
+        case 'bidaily':
+            output += Math.floor(now.getHours() / 12) + '_';
+        case 'daily':
+            output += now.getDay() + '_';
+        case 'weekly':
+            output += '_' + Math.floor(now.getDay() / 7);
+            break;
+    }
+    return output + now.getMonth() + '_' + now.getFullYear();
+}
+
 app.get('/package.zip', function(req, res){
+    var type = req.query.type || 'daily';
     fs.readFile('build/package.zip', function(err, data) {
         res.set('Content-Type', 'application/zip');
+        res.set('ETag', '"' + getETag(type, data) + '"');
         res.send(data);
     });
 });
