2d6356583d3b78a0ed8f29d675a67a4baf280475
Chris Van <cvan@mozilla.com>
2013-04-23 19:47:44 -0700

    fix several JS errors when trying to call overflow.init() on detail page

--- hearth/media/js/overflow.js
+++ hearth/media/js/overflow.js
@@ -1,15 +1,17 @@
 // Checks buttons for overflowing text and makes them wider if necessary.
 // - Currently only deals with infobox buttons.
 define('overflow', [], function() {
+    function init() {
+        // If this happens elsewhere we can target `.button` and use the
+        // .closest() parent to apply "overflowing" to.
+        var $infobox = $('.infobox.support');
 
-    // If this happens elsewhere we can target `.button` and use the
-    // .closest() parent to apply "overflowing" to.
-    var $infobox = $('.infobox.support');
-
-    $infobox.find('ul.c a').each(function() {
-        if (this.scrollWidth > $(this).innerWidth()) {
-            $infobox.addClass('overflowing');
-            return;
-        }
-    });
+        $infobox.find('ul.c a').each(function() {
+            if (this.scrollWidth > $(this).innerWidth()) {
+                $infobox.addClass('overflowing');
+                return;
+            }
+        });
+    }
+    return {init: init};
 });
