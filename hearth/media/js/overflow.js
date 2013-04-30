c48c5aa33892f261dbc21ad7c049cffc57fabd07
Matt Basta <me@mattbasta.com>
2013-04-29 17:31:49 -0700

    Accidentally removed too much; fixes tests

--- hearth/media/js/overflow.js
+++ hearth/media/js/overflow.js
@@ -1,14 +1,17 @@
 // Checks buttons for overflowing text and makes them wider if necessary.
 // - Currently only deals with infobox buttons.
 define('overflow', [], function() {
-    // If this happens elsewhere we can target `.button` and use the
-    // .closest() parent to apply "overflowing" to.
-    var $infobox = $('.infobox.support');
+    // We need this init. It's not called by marketplace.js, promise.
+    return {init: function() {
+        // If this happens elsewhere we can target `.button` and use the
+        // .closest() parent to apply "overflowing" to.
+        var $infobox = $('.infobox.support');
 
-    $infobox.find('ul.c a').each(function() {
-        if (this.scrollWidth > $(this).innerWidth()) {
-            $infobox.addClass('overflowing');
-            return;
-        }
-    });
+        $infobox.find('ul.c a').each(function() {
+            if (this.scrollWidth > $(this).innerWidth()) {
+                $infobox.addClass('overflowing');
+                return;
+            }
+        });
+    }};
 });
