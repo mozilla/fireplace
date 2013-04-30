edbeffd551ae6f5cec80a608e6616f4385e52aad
Matt Basta <me@mattbasta.com>
2013-04-29 17:01:48 -0700

    Update .po files for clouserw with the latest strings

--- hearth/media/js/settings_local_hosted.js
+++ hearth/media/js/settings_local_hosted.js
@@ -1,4 +1,3 @@
 define('settings_local', [], function() {
-    return {api_url: 'https://marketplace-dev.allizom.org'};
-    // return {api_url: window.location.origin};
+    return {api_url: window.location.origin};
 });
