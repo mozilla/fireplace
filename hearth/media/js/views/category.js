88b6c543faff8f54c733ef97e06dc43546b70e46
Matt Basta <me@mattbasta.com>
2013-04-30 17:50:17 -0700

    Use category models for category page titles

--- hearth/media/js/views/category.js
+++ hearth/media/js/views/category.js
@@ -1,14 +1,18 @@
 define('views/category',
-    ['capabilities', 'underscore', 'urls', 'utils', 'z'],
-    function(capabilities, _, urls, utils, z) {
+    ['capabilities', 'models', 'underscore', 'urls', 'utils', 'z'],
+    function(capabilities, models, _, urls, utils, z) {
     'use strict';
+
+    var cat_models = models('category');
 
     return function(builder, args, params) {
         var category = args[0];
         _.defaults(params || {}, {sort: 'popularity'});
 
+        var model = cat_models.lookup(category);
+
         builder.z('type', 'root');
-        builder.z('title', params.name || category);
+        builder.z('title', (model && model.name) || category);
         builder.z('show_cats', true);
         builder.z('cat', category);
 
