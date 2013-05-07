7f3babfbe72e0f4ccaa0d34fd8400bdb15209e78
Chris Van <cvan@mozilla.com>
2013-05-07 16:22:37 -0700

    fix Report Abuse because we do not need a common abuse.js view anymore

--- hearth/media/js/views/app/abuse.js
+++ hearth/media/js/views/app/abuse.js
@@ -1,8 +1,27 @@
-define('views/app/abuse', ['l10n'], function(l10n) {
+define('views/app/abuse',
+       ['forms', 'l10n', 'notification', 'requests', 'urls', 'z'],
+       function(forms, l10n, notification, requests, urls, z) {
 
     var gettext = l10n.gettext;
+    var notify = notification.notification;
 
-    // Form submission handled in views/abuse.js
+    z.page.on('submit', '.abuse-form', function(e) {
+        e.preventDefault();
+        // Submit report abuse form
+        var $this = $(this);
+        var slug = $this.find('input[name=app]').val();
+        var data = $this.serialize();
+
+        forms.toggleSubmitFormState($this);
+
+        requests.post($this.data('action'), data).done(function(data) {
+            notify({message: gettext('Abuse reported')});
+            z.page.trigger('navigate', urls.reverse('app', [slug]));
+        }).fail(function() {
+            forms.toggleSubmitFormState($this, true);
+            notify({message: gettext('Error while submitting report')});
+        });
+    });
 
     return function(builder, args) {
         builder.start('detail/abuse.html', {slug: args[0]});
