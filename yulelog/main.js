c29cf468305a04f817d142651b9daf95bf98fef5
Chris Van <cvan@mozilla.com>
2013-05-10 11:16:29 -0700

    add logging and catch possible exceptions from navigator.mozMobileConnection

--- yulelog/main.js
+++ yulelog/main.js
@@ -1,12 +1,18 @@
 (function() {
-  var conn = navigator.mozMobileConnection;
-  var qs = '';
-  if (conn) {
-    // `MCC`: Mobile Country Code
-    // `MNC`: Mobile Network Code
-    // `lastKnownNetwork`: `{MCC}-{MNC}`.
-    var network = (conn.lastKnownNetwork || conn.lastKnownHomeNetwork || '').split('-');
-    qs = '?mcc=' + (network[0] || '') + '&mnc=' + (network[1] || '');
+  try {
+    var conn = navigator.mozMobileConnection;
+    var qs = '';
+    if (conn) {
+      // `MCC`: Mobile Country Code
+      // `MNC`: Mobile Network Code
+      // `lastKnownNetwork`: `{MCC}-{MNC}`.
+      var network = (conn.lastKnownNetwork || conn.lastKnownHomeNetwork || '-').split('-');
+      qs = '?mcc=' + (network[0] || '') + '&mnc=' + (network[1] || '');
+      console.log('MCC: "' + network[0] + '"');
+      console.log('MNC: "' + network[1] + '"');
+    }
+  } catch(e) {
+    // Fail gracefully if `navigator.mozMobileConnection` gives us problems.
   }
   var i = document.createElement('iframe');
   i.seamless = true;
