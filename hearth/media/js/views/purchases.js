74f23a97b5d81cda9488aa9292e51e0349bef3cb
Matt Basta <me@mattbasta.com>
2013-05-13 16:31:20 -0700

    Add cache rewriting for app install (bug 870993)

--- hearth/media/js/views/purchases.js
+++ hearth/media/js/views/purchases.js
@@ -1,8 +1,26 @@
-define('views/purchases', ['l10n', 'urls', 'z'],
-    function(l10n, urls, z) {
+define('views/purchases', ['cache', 'l10n', 'urls', 'utils', 'z'],
+    function(cache, l10n, urls, utils, z) {
     'use strict';
 
     var gettext = l10n.gettext;
+
+    z.win.on('app_install_success', function(e, installer, product, installed_now) {
+        if (!installed_now) {
+            return;
+        }
+
+        var unsigned_url = urls.api.unsigned.url('installed');
+        cache.attemptRewrite(
+            function(key) {
+                return utils.baseurl(key) !== unsigned_url;
+            },
+            function(data) {
+                data.objects.push(product);  // TODO: convert this to use models
+                return data;
+            }
+        );
+
+    });
 
     return function(builder, args) {
         builder.start('user/purchases.html');
