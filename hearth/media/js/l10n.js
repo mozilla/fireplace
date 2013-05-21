c1e4b8e6eda06ddaee520a8dabfba82c603bb8e6
Chris Van <cvan@mozilla.com>
2013-05-21 09:33:55 -0700

    add French to our list of supported locales

--- hearth/media/js/l10n.js
+++ hearth/media/js/l10n.js
@@ -1,6 +1,6 @@
 (function() {
 
-var languages = ['cs', 'de', 'en-US', 'es', 'ga-IE', 'it', 'pl', 'pt-BR', 'zh-TW', 'dbg'];
+var languages = ['cs', 'de', 'en-US', 'es', 'fr', 'ga-IE', 'it', 'pl', 'pt-BR', 'zh-TW', 'dbg'];
 
 var lang_expander = {
     'en': 'en-US', 'ga': 'ga-IE',
