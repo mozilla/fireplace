cc4559680df6c9e64721818b1ec99f4888d876dc
Allen Short <ashort@mozilla.com>
2013-06-17 10:09:05 -0700

    Send region name to API when worldwide selected (bug 881278)

--- hearth/media/js/urls.js
+++ hearth/media/js/urls.js
@@ -91,9 +91,6 @@ define('urls',
                 device: _device(),
                 pro: buckets.get_profile()
             };
-            if (args.region === 'worldwide') {
-                delete args.region;
-            }
             if (user.logged_in()) {
                 args._user = user.get_token();
             }
