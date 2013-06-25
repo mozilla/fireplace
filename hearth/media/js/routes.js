8781944cd23f7cf7b1b6b53ad7150f71bc080c5c
Matt Basta <me@mattbasta.com>
2013-06-25 08:48:17 -0700

    Update routes for space heater

--- hearth/media/js/routes.js
+++ hearth/media/js/routes.js
@@ -2,31 +2,33 @@
 
 var dependencies;
 /* dtrace */
+
+// Please leave quotes around keys! They're needed for Space Heater.
 var routes = [
-    {pattern: '^/$', view_name: 'homepage'},
-    {pattern: '^/(index|server).html$', view_name: 'homepage'},
-    {pattern: '^/app/([^/<>"\']+)/ratings/add$', view_name: 'app/ratings/add'},
-    {pattern: '^/app/([^/<>"\']+)/ratings/edit$', view_name: 'app/ratings/edit'},
-    {pattern: '^/app/([^/<>"\']+)/ratings/([^/<>"\']+)$', view_name: 'app/ratings/rating'},
-    {pattern: '^/app/([^/<>"\']+)/ratings$', view_name: 'app/ratings'},
-    {pattern: '^/app/([^/<>"\']+)/abuse$', view_name: 'app/abuse'},
-    {pattern: '^/app/([^/<>"\']+)/privacy$', view_name: 'app/privacy'},
-    {pattern: '^/app/([^/<>"\']+)/receipt$', view_name: 'app/receipt'},
-    {pattern: '^/app/([^/<>"\']+)$', view_name: 'app'},
-    {pattern: '^/app/([^/<>"\']+)/$', view_name: 'app'},  // For the trailing slash
-    {pattern: '^/search$', view_name: 'search'},
-    {pattern: '^/search/$', view_name: 'search'},  // For the trailing slash
-    {pattern: '^/category/([^/<>"\']+)$', view_name: 'category'},
-    {pattern: '^/category/([^/<>"\']+)/featured$', view_name: 'featured'},
-    {pattern: '^/settings$', view_name: 'settings'},
-    {pattern: '^/feedback$', view_name: 'feedback'},
-    {pattern: '^/purchases$', view_name: 'purchases'},
+    {'pattern': '^/$', 'view_name': 'homepage'},
+    {'pattern': '^/(index|server).html$', 'view_name': 'homepage'},
+    {'pattern': '^/app/([^/<>"\']+)/ratings/add$', 'view_name': 'app/ratings/add'},
+    {'pattern': '^/app/([^/<>"\']+)/ratings/edit$', 'view_name': 'app/ratings/edit'},
+    {'pattern': '^/app/([^/<>"\']+)/ratings/([^/<>"\']+)$', 'view_name': 'app/ratings/rating'},
+    {'pattern': '^/app/([^/<>"\']+)/ratings$', 'view_name': 'app/ratings'},
+    {'pattern': '^/app/([^/<>"\']+)/abuse$', 'view_name': 'app/abuse'},
+    {'pattern': '^/app/([^/<>"\']+)/privacy$', 'view_name': 'app/privacy'},
+    {'pattern': '^/app/([^/<>"\']+)/receipt$', 'view_name': 'app/receipt'},
+    {'pattern': '^/app/([^/<>"\']+)$', 'view_name': 'app'},
+    {'pattern': '^/app/([^/<>"\']+)/$', 'view_name': 'app'},
+    {'pattern': '^/search$', 'view_name': 'search'},
+    {'pattern': '^/search/$', 'view_name': 'search'},
+    {'pattern': '^/category/([^/<>"\']+)$', 'view_name': 'category'},
+    {'pattern': '^/category/([^/<>"\']+)/featured$', 'view_name': 'featured'},
+    {'pattern': '^/settings$', 'view_name': 'settings'},
+    {'pattern': '^/feedback$', 'view_name': 'feedback'},
+    {'pattern': '^/purchases$', 'view_name': 'purchases'},
 
-    {pattern: '^/privacy-policy$', view_name: 'privacy'},
-    {pattern: '^/terms-of-use$', view_name: 'terms'},
+    {'pattern': '^/privacy-policy$', 'view_name': 'privacy'},
+    {'pattern': '^/terms-of-use$', 'view_name': 'terms'},
 
-    {pattern: '^/tests$', view_name: 'tests'},
-    {pattern: '^/debug$', view_name: 'debug'}
+    {'pattern': '^/tests$', 'view_name': 'tests'},
+    {'pattern': '^/debug$', 'view_name': 'debug'}
 ];
 
 dependencies = routes.map(function(i) {return 'views/' + i.view_name;});
