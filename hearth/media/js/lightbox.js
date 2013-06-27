a54eae917a4f3e3bdd2a75104cec5a86003a6c74
spasovski <davor@spasovski.net>
2013-06-27 14:47:16 -0700

    redo test 'fix' that was undone to test earlier

--- hearth/media/js/lightbox.js
+++ hearth/media/js/lightbox.js
@@ -151,7 +151,7 @@ define('lightbox',
         if (slider && slider.element) {
             slider.element.removeEventListener('fsmoveend', pauseVideos);
             slider.destroy();
-            //slider = null;
+            slider = null;
         }
     }
 
