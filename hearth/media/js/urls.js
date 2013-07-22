a1b5b5bfdad49a24b391ef051caebfb3d05a4f88
Matt Basta <me@mattbasta.com>
2013-07-22 16:42:27 -0700

    Externalize API routes

--- hearth/media/js/urls.js
+++ hearth/media/js/urls.js
@@ -1,6 +1,6 @@
 define('urls',
-    ['buckets', 'capabilities', 'format', 'settings', 'underscore', 'user', 'utils'],
-    function(buckets, caps, format, settings, _, user) {
+    ['buckets', 'capabilities', 'format', 'routes_api', 'settings', 'underscore', 'user', 'utils'],
+    function(buckets, caps, format, api_endpoints, settings, _, user) {
 
     var group_pattern = /\(.+\)/;
     var reverse = function(view_name, args) {
@@ -32,27 +32,6 @@ define('urls',
 
         }
         console.error('Could not find the view "' + view_name + '".');
-    };
-
-    var api_endpoints = {
-        'app': '/api/v1/fireplace/app/{0}/',
-        'app/privacy': '/api/v1/apps/app/{0}/privacy/',
-        'category': '/api/v1/fireplace/search/featured/?cat={0}',
-        'categories': '/api/v1/apps/category/',
-        'reviews': '/api/v1/apps/rating/',
-        'review': '/api/v1/apps/rating/{0}/',
-        'settings': '/api/v1/account/settings/mine/',
-        'installed': '/api/v1/account/installed/mine/',
-        'login': '/api/v1/account/login/',
-        'newsletter': '/api/v1/account/newsletter/',
-        'record_free': '/api/v1/installs/record/',
-        'record_paid': '/api/v1/receipts/install/',
-        'app_abuse': '/api/v1/abuse/app/',
-        'search': '/api/v1/apps/search/',
-        'feedback': '/api/v1/account/feedback/',
-
-        'prepare_nav_pay': '/api/v1/webpay/prepare/',
-        'payments_status': '/api/v1/webpay/status/{0}/'
     };
 
     function _dev() {
