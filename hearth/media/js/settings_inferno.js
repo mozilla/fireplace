fe7d6d0397309fdbd22055f2d33cc288cc722686
Matt Basta <me@mattbasta.com>
2013-09-03 16:31:01 -0700

    Change Inferno to point at Flue

--- hearth/media/js/settings_inferno.js
+++ hearth/media/js/settings_inferno.js
@@ -1,6 +1,7 @@
 define('settings_local', [], function() {
     return {
-        api_url: 'https://marketplace-dev.allizom.org',
+        //api_url: 'https://marketplace-dev.allizom.org',
+        api_url: 'http://flue.paas.allizom.org',
         body_classes: 'nightly'
     };
 });
