a8d979108761086ea02153f16b5843dba4ffb013
Matt Basta <me@mattbasta.com>
2013-09-04 08:13:06 -0700

    Remove unused deps

--- hearth/media/js/login.js
+++ hearth/media/js/login.js
@@ -1,6 +1,6 @@
 define('login',
-    ['cache', 'capabilities', 'defer', 'jquery', 'log', 'models', 'notification', 'settings', 'underscore', 'urls', 'user', 'requests', 'z'],
-    function(cache, capabilities, defer, $, log, models, notification, settings, _, urls, user, requests, z) {
+    ['capabilities', 'defer', 'jquery', 'log', 'notification', 'settings', 'underscore', 'urls', 'user', 'requests', 'z'],
+    function(capabilities, defer, $, log, notification, settings, _, urls, user, requests, z) {
 
     var console = log('login');
 
