30dc7201e02cfc9181e480ae1d794c8dae78c668
Matt Basta <me@mattbasta.com>
2013-10-05 22:28:16 -0700

    Fix urls.media() base path
    We only use this for easter eggs, anyway :-/

--- hearth/media/js/settings_local_hosted.js
+++ hearth/media/js/settings_local_hosted.js
@@ -3,6 +3,7 @@ define('settings_local', [], function() {
         window.location.protocol + '//' + window.location.host);
     return {
         api_url: origin,
+        media_url: document.body.getAttribute('data-media') + 'fireplace/',
         tracking_enabled: true,
         
         potatolytics_enabled: false
