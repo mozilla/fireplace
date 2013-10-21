27a01d2cf1d7beabfaa5be49f596a27e6f96da5a
Matt Basta <me@mattbasta.com>
2013-10-21 11:29:48 -0700

    Add shameful hack for Persona ToS/PP
    cc @cvan: If you update the docs, make sure to bump the cache bust in here now, as well.

--- hearth/media/js/marketplace.js
+++ hearth/media/js/marketplace.js
@@ -61,10 +61,19 @@ require.config({
         console.log('Dependencies resolved, starting init');
 
         var capabilities = require('capabilities');
+        var format = require('format');
         var nunjucks = require('templates');
         var settings = require('settings');
         var user = require('user');
         var z = require('z');
+
+        // Jank hack because Persona doesn't allow scripts in the doc iframe.
+        // Please just delete it when they don't do that anymore.
+        var doc_langs = ['el', 'en-US', 'es', 'pl', 'pt-BR'];
+        var doc_lang = doc_langs.indexOf(navigator.l10n.language) >= 0 ? navigator.l10n.language : 'en-US';
+        var doc_location = require('urls').media('/docs/{type}/' + doc_lang + '.html?20131014-4');
+        settings.persona_tos = format.format(doc_location, {type: 'terms'});
+        settings.persona_privacy = format.format(doc_location, {type: 'privacy'});
 
         nunjucks.env.dev = true;
 
