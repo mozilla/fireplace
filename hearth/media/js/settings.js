b45cc94eb77ade413d19346b91d0122a97586400
Chris Van <cvan@mozilla.com>
2013-11-26 13:43:42 -0500

    Revert "bump storage_version to invalid saved regions in localStorage (bug 941307)"
    This reverts commit 086627f65f532413803074b3162ee64e10bf64e6.

--- hearth/media/js/settings.js
+++ hearth/media/js/settings.js
@@ -18,7 +18,7 @@ define('settings', ['l10n', 'settings_local', 'underscore'], function(l10n, sett
 
         // The version number for localStorage data. Bump when the schema for
         // storing data in localStorage changes.
-        storage_version: '1',
+        storage_version: '0',
 
         // Set to true to simulate navigator.mozPay.
         simulate_nav_pay: false,
