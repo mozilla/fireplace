cf10c93f34368c8592c7842b6b5781e0474b0242
Matt Basta <me@mattbasta.com>
2013-12-04 12:51:53 -0800

    Add IT to list of docs

--- hearth/media/js/marketplace.js
+++ hearth/media/js/marketplace.js
@@ -77,7 +77,7 @@ require.config({
 
         // Jank hack because Persona doesn't allow scripts in the doc iframe.
         // Please just delete it when they don't do that anymore.
-        var doc_langs = ['el', 'en-US', 'es', 'pl', 'pt-BR', 'de'];
+        var doc_langs = ['el', 'en-US', 'es', 'it', 'pl', 'pt-BR', 'de'];
         var doc_lang = doc_langs.indexOf(navigator.l10n.language) >= 0 ? navigator.l10n.language : 'en-US';
         var doc_location = require('urls').media('/docs/{type}/' + doc_lang + '.html?20131014-4');
         settings.persona_tos = format.format(doc_location, {type: 'terms'});
