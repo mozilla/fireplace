512838e10124353ac1b1faea026276f67980c6fa
Matt Basta <me@mattbasta.com>
2013-12-05 18:48:37 -0800

    [highly experimental] Pin commonplace to a version
    `--force` because NPM

--- fabfile.py
+++ fabfile.py
@@ -23,7 +23,7 @@ def pre_update(ref):
 def update():
     with lcd(FIREPLACE):
         local('npm install')
-        local('npm install -g commonplace')
+        local('npm install --force commonplace@0.2.2')
         local('commonplace includes')
         local('commonplace langpacks')
 
