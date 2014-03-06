46103f6132c1a02cce7d10af1c80b56370a0d88b
Mathieu Pillard <mpillard@mozilla.com>
2014-03-06 18:34:16 +0100

    Add features debug page (bug 980409)

--- hearth/media/js/routes_api.js
+++ hearth/media/js/routes_api.js
@@ -18,6 +18,7 @@ define('routes_api', [], function() {
         'search': '/api/v1/apps/search/?cache=1&vary=0',
         'feedback': '/api/v1/account/feedback/',
         'consumer_info': '/api/v1/fireplace/consumer-info/',
+        'features': '/api/v1/apps/features/',
 
         'prepare_nav_pay': '/api/v1/webpay/prepare/',
         'payments_status': '/api/v1/webpay/status/{0}/'
