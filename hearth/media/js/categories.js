88036066da6e48b8f110b96532a31f67c5904f3e
Mathieu Pillard <mpillard@mozilla.com>
2014-03-19 16:18:55 +0100

    Add script to dump existing translations for categories to .po

--- hearth/media/js/categories.js
+++ hearth/media/js/categories.js
@@ -4,25 +4,25 @@ define('categories', ['l10n', 'models'], function(l10n, models) {
     var gettext = l10n.gettext;
     var catModels = models('category');
     var categories = [
-        { slug: 'all', name: gettext('All Categories') },
-        { slug: 'games', name: gettext('Games') },
-        { slug: 'books', name: gettext('Books') },
-        { slug: 'business', name: gettext('Business') },
-        { slug: 'education', name: gettext('Education') },
-        { slug: 'entertainment', name: gettext('Entertainment') },
-        { slug: 'health-fitness', name: gettext('Health & Fitness') },
-        { slug: 'lifestyle', name: gettext('Lifestyle') },
-        { slug: 'maps-navigation', name: gettext('Maps & Navigation') },
-        { slug: 'music', name: gettext('Music') },
-        { slug: 'news-weather', name: gettext('News & Weather') },
-        { slug: 'photo-video', name: gettext('Photo & Video') },
-        { slug: 'productivity', name: gettext('Productivity') },
-        { slug: 'reference', name: gettext('Reference') },
-        { slug: 'shopping', name: gettext('Shopping') },
-        { slug: 'social', name: gettext('Social') },
-        { slug: 'sports', name: gettext('Sports') },
-        { slug: 'travel', name: gettext('Travel') },
-        { slug: 'utilities', name: gettext('Utilities') }
+        {slug: 'all', name: gettext('All Categories')},
+        {slug: 'games', name: gettext('Games')},
+        {slug: 'books', name: gettext('Books')},
+        {slug: 'business', name: gettext('Business')},
+        {slug: 'education', name: gettext('Education')},
+        {slug: 'entertainment', name: gettext('Entertainment')},
+        {slug: 'health-fitness', name: gettext('Health & Fitness')},
+        {slug: 'lifestyle', name: gettext('Lifestyle')},
+        {slug: 'maps-navigation', name: gettext('Maps & Navigation')},
+        {slug: 'music', name: gettext('Music')},
+        {slug: 'news-weather', name: gettext('News & Weather')},
+        {slug: 'photo-video', name: gettext('Photo & Video')},
+        {slug: 'productivity', name: gettext('Productivity')},
+        {slug: 'reference', name: gettext('Reference')},
+        {slug: 'shopping', name: gettext('Shopping')},
+        {slug: 'social', name: gettext('Social')},
+        {slug: 'sports', name: gettext('Sports')},
+        {slug: 'travel', name: gettext('Travel')},
+        {slug: 'utilities', name: gettext('Utilities')}
     ];
     console.groupCollapsed('Casting categories to model cache...');
     catModels.cast(categories);
