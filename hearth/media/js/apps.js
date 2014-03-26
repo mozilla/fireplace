e0d45e1f48c4c5d6cf92ae4c9ad09135f5e270fd
Rob Hudson <chudson@mozilla.com>
2014-03-25 22:55:00 -0700

    This was actually right, just confusingly written

--- hearth/media/js/apps.js
+++ hearth/media/js/apps.js
@@ -138,7 +138,7 @@ define('apps',
             reasons.push(gettext('This app is unavailable for your platform.'));
         }
 
-        return product[COMPAT_REASONS] == reasons.length ? reasons : undefined;
+        return product[COMPAT_REASONS] = reasons.length ? reasons : undefined;
     }
     nunjucks.require('globals').app_incompat = incompat;
 
