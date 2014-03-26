2e1f8202f444f7d018b71cdf667929506cfc19e2
Wil Clouser <clouserw@gmail.com>
2014-03-26 08:57:16 -0700

    Revert "defer image loading on search listing (bug 898320)"
    This reverts commit 828cc13362faf2d0fd0b816ef3acd58f7f60659b.

--- hearth/media/js/views/search.js
+++ hearth/media/js/views/search.js
@@ -1,11 +1,9 @@
 define('views/search',
-    ['capabilities', 'image-deferrer', 'l10n', 'storage', 'tracking', 'underscore', 'urls', 'utils', 'z'],
-    function(capabilities, ImageDeferrer, l10n, storage, tracking, _, urls, utils, z) {
+    ['capabilities', 'l10n', 'storage', 'tracking', 'underscore', 'urls', 'utils', 'z'],
+    function(capabilities, l10n, storage, tracking, _, urls, utils, z) {
 
     var _pd = utils._pd;
     var gettext = l10n.gettext;
-    var iconDeferrer = ImageDeferrer.Deferrer(100);
-    var screenshotDeferrer = ImageDeferrer.Deferrer(null, 200);
 
     function append(existing_value, new_value) {
         if (typeof existing_value === 'string' && existing_value !== '') {
@@ -133,8 +131,6 @@ define('views/search',
             'click',
             expand ? 'Expanded view' : 'List view'
         );
-
-        z.doc.trigger('scroll');  // For defer image loading.
     })).on('submit', 'form#search', function(e) {
         e.stopPropagation();
         e.preventDefault();
@@ -161,8 +157,6 @@ define('views/search',
         // If this is a search results or "my apps" page.
         if (isSearchPage()) {
             setTrays(expand);
-            iconDeferrer.setImages($('.mkt-tile .icon'));
-            screenshotDeferrer.setImages($('.screenshot img'));
         }
     }).on('reloaded_chrome', function() {
         if (isSearchPage()) {
@@ -172,9 +166,6 @@ define('views/search',
         z.page.trigger('populatetray');
         // Update "Showing 1-{total}" text.
         z.page.find('.total-results').text(z.page.find('.item.app').length);
-
-        screenshotDeferrer.refresh();
-        iconDeferrer.refresh();
     }).on('search', function(e, params) {
         e.preventDefault();
         return z.page.trigger(
