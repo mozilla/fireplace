540f3a965098baaa34541d1c2cbbdf3eb83847e0
Wil Clouser <clouserw@gmail.com>
2014-03-26 08:57:15 -0700

    Revert "defer icons + screenshots everywhere, fade-ins and less janky redraws"
    This reverts commit da8a5d61132dbb62d91c546cf5fc85cd6ad4c8a2.

--- hearth/media/js/marketplace.js
+++ hearth/media/js/marketplace.js
@@ -204,29 +204,5 @@ function(_) {
         throw new Error('Cancel navigation; deprecated client');
     });
 
-    var ImageDeferrer = require('image-deferrer');
-    var iconDeferrer = ImageDeferrer.Deferrer(100, null);
-    var screenshotDeferrer = ImageDeferrer.Deferrer(null, 200);
-    z.page.one('loaded', function() {
-        iconDeferrer.setImages($('.mkt-tile .icon.deferred'));
-        screenshotDeferrer.setImages($('.screenshot img.deferred'));
-    }).on('loaded loaded_more', function() {
-        iconDeferrer.refresh();
-        screenshotDeferrer.refresh();
-    });
-    nunjucks_globals.imgAlreadyDeferred = function(src) {
-        /*
-            If an image already has been loaded, we use this helper in case the
-            view is triggered to be rebuilt. When pages are rebuilt, we don't
-            mark images to be deferred if they have already been loaded.
-            This fixes images flashing back to the placeholder image when
-            switching between the New and Popular tabs on the home page.
-        */
-        var iconsLoaded = iconDeferrer.getSrcsAlreadyLoaded();
-        var screenshotsLoaded = screenshotDeferrer.getSrcsAlreadyLoaded();
-        var loaded = iconsLoaded.concat(screenshotsLoaded);
-        return loaded.indexOf(src) !== -1;
-    };
-
     console.log('Initialization complete');
 });
