3d32cb5065f18c4ea037d0943ef9e6f168108c9f
Christopher Van <cvan@mozilla.com>
2014-03-31 18:29:41 -0700

    bump commonplace: remove assert.js and views/tests.js from minified JS bundle (bug 990319)

--- fabfile.py
+++ fabfile.py
@@ -24,7 +24,7 @@ def pre_update(ref):
 def update():
     with lcd(FIREPLACE):
         local('npm install')
-        local('npm install --force commonplace@0.2.8')
+        local('npm install --force commonplace@0.2.9')
         local('%s includes' % COMMONPLACE)
         local('%s langpacks' % COMMONPLACE)
 
