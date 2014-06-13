577145dde2773b4e65f64a2bdc91057f3e82fa18
Jason Thomas <jason@lithiumfox.com>
2014-06-13 12:29:03 -0400

    fabfile: add build_package and upload_package for bug 1020500

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,17 @@ import deploysettings as settings
 env.key_filename = settings.SSH_KEY
 fabdeploytools.envs.loadenv(settings.CLUSTER)
 ROOT, FIREPLACE = helpers.get_app_dirs(__file__)
-COMMONPLACE = '%s/node_modules/commonplace/bin/commonplace' % FIREPLACE
+
+COMMONPLACE = '%s/node_modules/commonplace/bin' % FIREPLACE
+GRUNT = '%s/node_modules/grunt-cli/bin' % FIREPLACE
+
+if settings.ZAMBONI_DIR:
+    ZAMBONI = '%s/zamboni' % settings.ZAMBONI_DIR
+    ZAMBONI_PYTHON = '%s/venv/bin/python' % settings.ZAMBONI_DIR
+    FIREPLACE_PACKAGE = '%s/package/archives/latest_%s.zip' % (FIREPLACE,
+                                                               settings.ENV)
+
+os.environ["PATH"] += os.pathsep + os.pathsep.join([COMMONPLACE, GRUNT])
 
 
 @task
@@ -25,12 +35,15 @@ def update():
     with lcd(FIREPLACE):
         local('npm install')
         local('npm install --force commonplace@0.3.2')
-        local('%s includes' % COMMONPLACE)
-        local('%s langpacks' % COMMONPLACE)
+        local('commonplace includes')
+        local('commonplace langpacks')
 
 
 @task
 def deploy():
+    if settings.ZAMBONI_DIR:
+        build_package()
+        upload_package()
     helpers.deploy(name=settings.PROJECT_NAME,
                    app_dir='fireplace',
                    env=settings.ENV,
@@ -51,3 +64,17 @@ def pre_update_latest_tag():
             f.seek(0)
             f.write(latest_tag)
             f.truncate()
+
+
+@task
+def build_package():
+    with lcd(FIREPLACE):
+        local('make package_%s' % settings.ENV)
+
+
+@task
+def upload_package():
+    with lcd(ZAMBONI):
+        local('%s manage.py --settings=settings_local_mkt '
+              'upload_new_marketplace_package %s %s '
+              % (ZAMBONI_PYTHON, 'marketplace', FIREPLACE_PACKAGE))
