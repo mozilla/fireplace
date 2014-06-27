fb7f92c0ada0339a1ca9918aef37388933e84fb7
Allen Short <ashort@mozilla.com>
2014-06-27 12:34:43 -0700

    missed file

---
+++ hearth/media/js/views/fxa_authorize.js
@@ -0,0 +1,8 @@
+define('views/fxa_authorize', [], function() {
+    'use strict';
+
+    return function(builder) {
+      window.opener.postMessage({auth_code: window.location.toString()}, "*");
+      window.close();
+    }
+});
