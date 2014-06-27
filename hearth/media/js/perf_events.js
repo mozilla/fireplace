636d682f7d6393a5dd3654f4c5e60ebe7b11d17a
Kevin Ngo <ngoke@mozilla.com>
2014-06-26 17:09:38 -0700

    fix perf events test for phantom

--- hearth/media/js/perf_events.js
+++ hearth/media/js/perf_events.js
@@ -6,22 +6,22 @@ define('perf_events', ['z'], function(z) {
     z.page.one('reloaded_chrome', function() {
         // Emit this event when your application designates that its core
         // chrome or navigation interface exists in the DOM.
-        window.dispatchEvent(new CustomEvent('moz-chrome-interactive'));
+        z.win.trigger('moz-chrome-interactive');
     })
     .one('loaded', function() {
         // Emit this event when your application designates that it has bound
         // the events for the minimum set of functionality to allow the user to
         // interact with the "above-the-fold" content.
-        window.dispatchEvent(new CustomEvent('moz-content-interactive'));
+        z.win.trigger('moz-content-interactive');
     })
     .one('splash_removed', function() {
         // Only once splash is removed can stuff be done.
-        window.dispatchEvent(new CustomEvent('moz-chrome-dom-loaded'));
-        window.dispatchEvent(new CustomEvent('moz-app-visually-complete'));
+        z.win.trigger('moz-chrome-dom-loaded');
+        z.win.trigger('moz-app-visually-complete');
     })
     .one('images_loaded', function() {
         // Emit this event when your application has been completely
         // loaded.
-        window.dispatchEvent(new CustomEvent('moz-app-loaded'));
+        z.win.trigger('moz-app-loaded');
     });
 });
