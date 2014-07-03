ca5303732823a6cdf2a69ff5c7d6edee7aae319a
Kevin Ngo <ngoke@mozilla.com>
2014-07-03 11:07:12 -0700

    use dev/stage iframe installer for dev/stage packages

--- hearth/media/js/settings_package_dev.js
+++ hearth/media/js/settings_package_dev.js
@@ -2,6 +2,7 @@ define('settings_local', [], function() {
     return {
         api_url: 'https://marketplace-dev.allizom.org',
         body_classes: 'package',
+        iframe_installer_src: 'https://marketplace-dev.allizom.org/iframe-install.html',
         media_url: 'https://marketplace-dev.mozflare.net/media/',
         tracking_enabled: true,
         potatolytics_enabled: true,
