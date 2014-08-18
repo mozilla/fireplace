b7dfdae5b55552310c8e1217ffa802f86fd82a7c
Mathieu Pillard <m@virgule.net>
2014-08-18 18:07:18 +0200

    Enable device filtering on desktop (bug 1054981)

--- src/media/js/routes_api_args.js
+++ src/media/js/routes_api_args.js
@@ -11,6 +11,8 @@ define('routes_api_args',
     } else if (caps.firefoxAndroid) {
         _dev = 'android';
         _device = caps.widescreen() ? 'tablet' : 'mobile';
+    } else {
+        _dev = _device = 'desktop';
     }
 
     if (_device == 'mobile' || _device == 'firefoxos') {
