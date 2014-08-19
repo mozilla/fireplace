60645e2d7fa02f86075cfc135cf5eb6b2a61cbda
Kevin Ngo <ngoke@mozilla.com>
2014-08-18 17:11:26 -0700

    fix filter on pagination, brands showing up with 4 apps (bug 1055331)

--- src/media/js/helpers_local.js
+++ src/media/js/helpers_local.js
@@ -19,6 +19,9 @@ define('helpers_local',
 
     /* Register filters. */
     filters.json = JSON.stringify;
+    // TODO: figure out why these filters aren't being registered into
+    // nunjucks.env but helpers.js's filters are.
+    nunjucks.env.addFilter('json', JSON.stringify);
 
     filters.items = utils_local.items;
 
