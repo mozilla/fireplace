mozilla/fireplace

commit 83c57f0a286b988f3f95871561f5a833798bd875

Upstream changes from feed branch, getting them in here so they can be localized in advance of merge (bug 1055851).

src/media/js/feed.js
@@ -1,67 +1,8 @@
 /* This is a shared file between Fireplace and Transonic. */
 define('feed',
-    ['l10n', 'models', 'nunjucks', 'underscore', 'utils_local'], function(l10n, models, nunjucks, _, utils_local) {
+    ['edbrands', 'l10n', 'models', 'nunjucks', 'underscore', 'utils_local'], function(edbrands, l10n, models, nunjucks, _, utils_local) {
     'use strict';
     var gettext = l10n.gettext;
-
-    var BRAND_TYPES = {
-        'apps-for-albania': [gettext('App for Albania'), gettext('Apps for Albania')],
-        'apps-for-argentina': [gettext('App for Argentina'), gettext('Apps for Argentina')],
-        'apps-for-bangladesh': [gettext('App for Bangladesh'), gettext('Apps for Bangladesh')],
-        'apps-for-brazil': [gettext('App for Brazil'), gettext('Apps for Brazil')],
-        'apps-for-bulgaria': [gettext('App for Bulgaria'), gettext('Apps for Bulgaria')],
-        'apps-for-chile': [gettext('App for Chile'), gettext('Apps for Chile')],
-        'apps-for-china': [gettext('App for China'), gettext('Apps for China')],
-        'apps-for-colombia': [gettext('App for Colombia'), gettext('Apps for Colombia')],
-        'apps-for-costa-rica': [gettext('App for Costa Rica'), gettext('Apps for Costa Rica')],
-        'apps-for-croatia': [gettext('App for Croatia'), gettext('Apps for Croatia')],
-        'apps-for-czech-republic': [gettext('App for Czech Republic'), gettext('Apps for Czech Republic')],
-        'apps-for-ecuador': [gettext('App for Ecuador'), gettext('Apps for Ecuador')],
-        'apps-for-el-salvador': [gettext('App for El Salvador'), gettext('Apps for El Salvador')],
-        'apps-for-france': [gettext('App for France'), gettext('Apps for France')],
-        'apps-for-germany': [gettext('App for Germany'), gettext('Apps for Germany')],
-        'apps-for-greece': [gettext('App for Greece'), gettext('Apps for Greece')],
-        'apps-for-hungary': [gettext('App for Hungary'), gettext('Apps for Hungary')],
-        'apps-for-india': [gettext('App for India'), gettext('Apps for India')],
-        'apps-for-italy': [gettext('App for Italy'), gettext('Apps for Italy')],
-        'apps-for-japan': [gettext('App for Japan'), gettext('Apps for Japan')],
-        'apps-for-macedonia': [gettext('App for Macedonia'), gettext('Apps for Macedonia')],
-        'apps-for-mexico': [gettext('App for Mexico'), gettext('Apps for Mexico')],
-        'apps-for-montenegro': [gettext('App for Montenegro'), gettext('Apps for Montenegro')],
-        'apps-for-nicaragua': [gettext('App for Nicaragua'), gettext('Apps for Nicaragua')],
-        'apps-for-panama': [gettext('App for Panama'), gettext('Apps for Panama')],
-        'apps-for-peru': [gettext('App for Peru'), gettext('Apps for Peru')],
-        'apps-for-poland': [gettext('App for Poland'), gettext('Apps for Poland')],
-        'apps-for-russia': [gettext('App for Russia'), gettext('Apps for Russia')],
-        'apps-for-serbia': [gettext('App for Serbia'), gettext('Apps for Serbia')],
-        'apps-for-south-africa': [gettext('App for South Africa'), gettext('Apps for South Africa')],
-        'apps-for-spain': [gettext('App for Spain'), gettext('Apps for Spain')],
-        'apps-for-uruguay': [gettext('App for Uruguay'), gettext('Apps for Uruguay')],
-        'apps-for-venezuela': [gettext('App for Venezuela'), gettext('Apps for Venezuela')],
-        'arts-entertainment': [gettext('Arts & Entertainment'), gettext('Arts & Entertainment')],
-        'book': [gettext('Book'), gettext('Books')],
-        'creativity': [gettext('Creativity'), gettext('Creativity')],
-        'education': [gettext('Education'), gettext('Education')],
-        'games': [gettext('Game'), gettext('Games')],
-        'groundbreaking': [gettext('Groundbreaking'), gettext('Groundbreaking')],
-        'health-fitness': [gettext('Health & Fitness'), gettext('Health & Fitness')],
-        'hidden-gem': [gettext('Hidden Gem'), gettext('Hidden Gems')],
-        'lifestyle': [gettext('Lifestyle'), gettext('Lifestyle')],
-        'local-favorite': [gettext('Local Favorite'), gettext('Local Favorites')],
-        'maps-navigation': [gettext('Maps & Navigation'), gettext('Maps & Navigation')],
-        'music': [gettext('Music'), gettext('Music')],
-        'mystery-app': [gettext('Mystery App!'), gettext('Mystery Apps!')],
-        'news-weather': [gettext('News & Weather'), gettext('News & Weather')],
-        'photo-video': [gettext('Photo & Video'), gettext('Photo & Video')],
-        'shopping': [gettext('Shopping'), gettext('Shopping')],
-        'social': [gettext('Social'), gettext('Social')],
-        'sports': [gettext('Sports'), gettext('Sports')],
-        'tools-time-savers': [gettext('Tools & Time Saver'), gettext('Tools & Time Savers')],
-        'travel': [gettext('Travel'), gettext('Travel')],
-        'work-business': [gettext('Work & Business'), gettext('Work & Business')]
-    };
-
-    var BRAND_TYPES_CHOICES = utils_local.items(BRAND_TYPES);
 
     var BRAND_LAYOUTS = {
         'grid': gettext('Grid Layout'),
@@ -103,13 +44,6 @@ define('feed',
         'listing': gettext('Listing Collection'),
     };
 
-    function get_brand_name(item) {
-        if (item.apps.length > 1) {
-            return BRAND_TYPES[item.type][1];
-        }
-        return BRAND_TYPES[item.type][0];
-    }
-
     function get_brand_color_class(brand) {
         /*
         Passed the JSON representation of an editorial brand, returns a random
@@ -120,7 +54,7 @@ define('feed',
             // Generate a unique identifier from the brand.
             var brand_id = brand.type;
             _.each(brand.apps, function(app) {
-                brand_id += '_' + app.slug;
+                brand_id += '_' + app.slug
             });
             return brand_id;
         }
@@ -180,8 +114,7 @@ define('feed',
     var MAX_BRAND_APPS = 6;
 
     return {
-        BRAND_TYPES: BRAND_TYPES,
-        BRAND_TYPES_CHOICES: BRAND_TYPES_CHOICES,
+        BRAND_TYPES: edbrands.BRAND_TYPES,
         BRAND_LAYOUTS: BRAND_LAYOUTS,
         BRAND_LAYOUTS_CHOICES: BRAND_LAYOUTS_CHOICES,
         COLL_PROMO: COLL_PROMO,
@@ -199,7 +132,7 @@ define('feed',
         cast_collection: models('feed-collection').cast,
         cast_shelf: models('feed-shelf').cast,
         get_brand_color_class: get_brand_color_class,
-        get_brand_name: get_brand_name,
+        get_brand_type: edbrands.get_brand_type,
         group_apps: group_apps,
         MAX_BRAND_APPS: MAX_BRAND_APPS,
     };

src/media/js/edbrands.js
@@ -1,0 +1,98 @@
+/* This is a shared file between Fireplace and Transonic. */
+define('edbrands',
+    ['l10n'], function(l10n) {
+    'use strict';
+    var ngettext = l10n.ngettext;
+
+    function get_brand_type(slug, numApps) {
+        /*
+        Passed the slug of an editorial brand and the number of apps it
+        contains, will return the translated name of that brand.
+
+        If no arguments are passed, will returns an array of all slugs of all
+        editorial brands.
+
+        Example (assuming en_US is the active language):
+        >>> get_brand_type()
+        ['apps-for-albania', 'apps-for-argentina', ...]
+        >>> get_brand_type('hidden-gem', 1)
+        'Hidden Gem'
+        >>> get_brand_type('hidden-gem', 2)
+        'Hidden Gems'
+        */
+
+        if (!arguments.length) {
+            slug = null;
+            numApps = 1;
+        }
+
+        // Do not change these translations unless you know what you're doing;
+        // you will almost certainly muck up the alignment with their translations.
+        // If you wish to change how they are displayed to users, use Verbatim.
+        var brand_types = {
+            'apps-for-albania': ngettext('App for Albania', 'Apps for Albania', {n: numApps}),
+            'apps-for-argentina': ngettext('App for Argentina', 'Apps for Argentina', {n: numApps}),
+            'apps-for-bangladesh': ngettext('App for Bangladesh', 'Apps for Bangladesh', {n: numApps}),
+            'apps-for-brazil': ngettext('App for Brazil', 'Apps for Brazil', {n: numApps}),
+            'apps-for-bulgaria': ngettext('App for Bulgaria', 'Apps for Bulgaria', {n: numApps}),
+            'apps-for-chile': ngettext('App for Chile', 'Apps for Chile', {n: numApps}),
+            'apps-for-china': ngettext('App for China', 'Apps for China', {n: numApps}),
+            'apps-for-colombia': ngettext('App for Colombia', 'Apps for Colombia', {n: numApps}),
+            'apps-for-costa-rica': ngettext('App for Costa Rica', 'Apps for Costa Rica', {n: numApps}),
+            'apps-for-croatia': ngettext('App for Croatia', 'Apps for Croatia', {n: numApps}),
+            'apps-for-czech-republic': ngettext('App for Czech Republic', 'Apps for Czech Republic', {n: numApps}),
+            'apps-for-ecuador': ngettext('App for Ecuador', 'Apps for Ecuador', {n: numApps}),
+            'apps-for-el-salvador': ngettext('App for El Salvador', 'Apps for El Salvador', {n: numApps}),
+            'apps-for-france': ngettext('App for France', 'Apps for France', {n: numApps}),
+            'apps-for-germany': ngettext('App for Germany', 'Apps for Germany', {n: numApps}),
+            'apps-for-greece': ngettext('App for Greece', 'Apps for Greece', {n: numApps}),
+            'apps-for-hungary': ngettext('App for Hungary', 'Apps for Hungary', {n: numApps}),
+            'apps-for-india': ngettext('App for India', 'Apps for India', {n: numApps}),
+            'apps-for-italy': ngettext('App for Italy', 'Apps for Italy', {n: numApps}),
+            'apps-for-japan': ngettext('App for Japan', 'Apps for Japan', {n: numApps}),
+            'apps-for-macedonia': ngettext('App for Macedonia', 'Apps for Macedonia', {n: numApps}),
+            'apps-for-mexico': ngettext('App for Mexico', 'Apps for Mexico', {n: numApps}),
+            'apps-for-montenegro': ngettext('App for Montenegro', 'Apps for Montenegro', {n: numApps}),
+            'apps-for-nicaragua': ngettext('App for Nicaragua', 'Apps for Nicaragua', {n: numApps}),
+            'apps-for-panama': ngettext('App for Panama', 'Apps for Panama', {n: numApps}),
+            'apps-for-peru': ngettext('App for Peru', 'Apps for Peru', {n: numApps}),
+            'apps-for-poland': ngettext('App for Poland', 'Apps for Poland', {n: numApps}),
+            'apps-for-russia': ngettext('App for Russia', 'Apps for Russia', {n: numApps}),
+            'apps-for-serbia': ngettext('App for Serbia', 'Apps for Serbia', {n: numApps}),
+            'apps-for-south-africa': ngettext('App for South Africa', 'Apps for South Africa', {n: numApps}),
+            'apps-for-spain': ngettext('App for Spain', 'Apps for Spain', {n: numApps}),
+            'apps-for-uruguay': ngettext('App for Uruguay', 'Apps for Uruguay', {n: numApps}),
+            'apps-for-venezuela': ngettext('App for Venezuela', 'Apps for Venezuela', {n: numApps}),
+            'arts-entertainment': ngettext('Arts & Entertainment App', 'Arts & Entertainment Apps', {n: numApps}),
+            'book': ngettext('Featured Book App', 'Featured Book Apps', {n: numApps}),
+            'creativity': ngettext('Featured Creativity App', 'Featured Creativity Apps', {n: numApps}),
+            'education': ngettext('Featured Education App', 'Featured Education Apps', {n: numApps}),
+            'games': ngettext('Featured Game', 'Featured Games', {n: numApps}),
+            'groundbreaking': ngettext('Groundbreaking App', 'Groundbreaking Apps', {n: numApps}),
+            'health-fitness': ngettext('Health & Fitness App', 'Health & Fitness Apps', {n: numApps}),
+            'hidden-gem': ngettext('Hidden Gem', 'Hidden Gems', {n: numApps}),
+            'lifestyle': ngettext('Featured Lifestyle App', 'Featured Lifestyle Apps', {n: numApps}),
+            'local-favorite': ngettext('Local Favorite App', 'Local Favorite Apps', {n: numApps}),
+            'maps-navigation': ngettext('Maps & Navigation App', 'Maps & Navigation Apps', {n: numApps}),
+            'music': ngettext('Featured Music App', 'Featured Music Apps', {n: numApps}),
+            'mystery-app': ngettext('Mystery App!', 'Mystery Apps!', {n: numApps}),
+            'news-weather': ngettext('News & Weather App', 'News & Weather Apps', {n: numApps}),
+            'photo-video': ngettext('Photo & Video App', 'Photo & Video Apps', {n: numApps}),
+            'shopping': ngettext('Featured Shopping App', 'Featured Shopping Apps', {n: numApps}),
+            'social': ngettext('Featured Social App', 'Featured Social Apps', {n: numApps}),
+            'sports': ngettext('Featured Sports App', 'Featured Sports Apps', {n: numApps}),
+            'tools-time-savers': ngettext('Tool & Time Saver', 'Tools & Time Savers', {n: numApps}),
+            'travel': ngettext('Featured Travel App', 'Featured Travel Apps', {n: numApps}),
+            'work-business': ngettext('Work & Business App', 'Work & Business Apps', {n: numApps})
+        };
+        if (slug) {
+            return brand_types[slug];
+        }
+        return Object.keys(brand_types);
+    }
+
+    return {
+        BRAND_TYPES: get_brand_type(),
+        get_brand_type: get_brand_type,
+    };
+});

commit 7608c71a5927190ef89f9d7ca4e5b1c04973a822

Fix linting error introduced with 83c57f0a286b988f3f95871561f5a833798bd875

src/media/js/feed.js
@@ -54,7 +54,7 @@ define('feed',
             // Generate a unique identifier from the brand.
             var brand_id = brand.type;
             _.each(brand.apps, function(app) {
-                brand_id += '_' + app.slug
+                brand_id += '_' + app.slug;
             });
             return brand_id;
         }