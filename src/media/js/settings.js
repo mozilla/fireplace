0b46d8aae47e1f0ec627a838c0623a187cef60a4
Rob Hudson <chudson@mozilla.com>
2014-09-12 10:18:13 -0700

    Added new region (bug 1050213)

--- src/media/js/settings.js
+++ src/media/js/settings.js
@@ -120,6 +120,7 @@ define('settings', ['l10n', 'settings_local', 'underscore'], function(l10n, sett
             'hu': gettext('Hungary'),
             'in': gettext('India'),
             'it': gettext('Italy'),
+            'jp': gettext('Japan'),
             'mx': gettext('Mexico'),
             'me': gettext('Montenegro'),
             'ni': gettext('Nicaragua'),
