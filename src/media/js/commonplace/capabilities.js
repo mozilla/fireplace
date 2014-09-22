b362e50e36ccf8aecc656baa881aca38fbaa798b
Allen Short <ashort@mozilla.com>
2014-09-22 10:48:57 -0700

    Require 2.1 for native FxA (bug 1070269)

--- src/media/js/commonplace/capabilities.js
+++ src/media/js/commonplace/capabilities.js
@@ -36,7 +36,7 @@ define('capabilities', ['settings'], function(settings) {
     static_caps.nativeFxA = function() {
         return (static_caps.firefoxOS &&
                 window.location.protocol === 'app:' &&
-                navigator.userAgent.match(/rv:(\d{2})/)[1] >= 32);
+                navigator.userAgent.match(/rv:(\d{2})/)[1] >= 34);
 
     };
     static_caps.fallbackFxA = function() {
