db1c6f3ec39941d57f9565dcf7b780f78b896f76
Allen Short <ashort@mozilla.com>
2014-09-24 10:53:59 -0700

    clever fix for packaged app fxa web flow

--- src/media/js/views/fxa_authorize.js
+++ src/media/js/views/fxa_authorize.js
@@ -4,6 +4,12 @@ define('views/fxa_authorize', [], function() {
     return function(builder) {
       window.opener.postMessage({auth_code: window.location.toString()},
                                 window.location.origin);
+      // This code will execute from a hosted origin, since the FxA login
+      // process redirects there. Nevertheless, this might be a window opened by
+      // the marketplace packaged app. So, let's send this info to it, if so.
+      window.opener.postMessage({auth_code: window.location.toString()},
+                                "app://packaged." + window.location.host);
+
       window.close();
     };
 });
