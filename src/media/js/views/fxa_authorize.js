49759bbea0463c9207ff4682b1a1819731265ad6
Allen Short <ashort@mozilla.com>
2014-09-24 17:19:02 -0700

    forget failed fxa fix

--- src/media/js/views/fxa_authorize.js
+++ src/media/js/views/fxa_authorize.js
@@ -4,14 +4,6 @@ define('views/fxa_authorize', ['log'], function(log) {
     return function(builder) {
       window.opener.postMessage({auth_code: window.location.toString()},
                                 window.location.origin);
-      // This code will execute from a hosted origin, since the FxA login
-      // process redirects there. Nevertheless, this might be a window opened by
-      // the marketplace packaged app. So, let's send this info to it, if so.
-      var packaged_origin = "app://packaged." + window.location.host;
-      console.log("posting oauth code to " + packaged_origin);
-      window.opener.postMessage({auth_code: window.location.toString()},
-                                packaged_origin);
-
       window.close();
     };
 });
