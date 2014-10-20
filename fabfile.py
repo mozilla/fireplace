4eb8bdad0ad518d1e8bcdfeddf96205184cfe08d
Kevin Ngo <ngoke@mozilla.com>
2014-10-20 16:57:01 -0700

    use npm install over update

--- fabfile.py
+++ fabfile.py
@@ -33,7 +33,7 @@ def pre_update(ref):
 @task
 def update():
     with lcd(FIREPLACE):
-        local('npm update')
+        local('npm install')
         local('node_modules/.bin/bower update --allow-root')
         local('make update')
         local('cp src/media/js/settings_local_hosted.js src/media/js/settings_local.js')
