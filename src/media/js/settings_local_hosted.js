fa8fafff1d2684fd00fd7ab945f7ad2c89fd3b78
Stuart Colville <scolville@mozilla.com>
2014-11-05 16:57:02 +0000

    Use commonplace polyfill for window.location.origin (bug 1094175)

--- src/media/js/settings_local_hosted.js
+++ src/media/js/settings_local_hosted.js
@@ -1,8 +1,6 @@
 define('settings_local', [], function() {
-    var origin = window.location.origin || (
-        window.location.protocol + '//' + window.location.host);
     return {
-        api_url: origin,
+        api_url: window.location.origin,
         media_url: document.body.getAttribute('data-media'),
         iframe_installer_src: origin + '/iframe-install.html',
         iframe_potatolytics_src: origin + '/potatolytics.html',
