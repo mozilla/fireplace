da86052ae580616723669b57b410bcc90a0cc65f
Kevin Ngo <ngoke@mozilla.com>
2014-11-06 03:08:56 -0800

    add npm install to fabfile

--- fabfile.py
+++ fabfile.py
@@ -33,6 +33,7 @@ def pre_update(ref):
 @task
 def update():
     with lcd(FIREPLACE):
+        local('npm install')
         local('make update')
         local('cp src/media/js/settings_local_hosted.js src/media/js/settings_local.js')
 
