db666e86b3deb6eebfa2c1de578ae8a00bfd6713
Kevin Ngo <ngoke@mozilla.com>
2014-12-22 14:57:43 -0800

    refresh abuse modal (bug 1114795)

--- src/media/js/views/app/abuse.js
+++ src/media/js/views/app/abuse.js
@@ -1,13 +1,13 @@
 define('views/app/abuse',
-       ['forms', 'l10n', 'notification', 'requests', 'urls', 'utils', 'z'],
-       function(forms, l10n, notification, requests, urls, utils, z) {
-
+    ['capabilities', 'forms', 'l10n', 'notification', 'nunjucks',  'requests',
+     'urls', 'utils', 'z'],
+    function(caps, forms, l10n, notification, nunjucks, requests, urls,
+             utils, z) {
     var gettext = l10n.gettext;
     var notify = notification.notification;
 
-    z.page.on('submit', '.abuse-form', function(e) {
-        e.preventDefault();
-        // Submit report abuse form
+    z.doc.on('submit', '.abuse-form', utils._pd(function(e) {
+        // Used for both the report abuse page and the modal.
         var $this = $(this);
         var slug = $this.find('input[name=app]').val();
         var data = utils.getVars($this.serialize());
@@ -15,22 +15,43 @@ define('views/app/abuse',
         forms.toggleSubmitFormState($this);
 
         requests.post(urls.api.url('app_abuse'), data).done(function(data) {
-            notify({message: gettext('Abuse reported')});
-            z.page.trigger('navigate', urls.reverse('app', [slug]));
+            notify({message: gettext('Abuse report submitted. Thanks!')});
+            if (!caps.widescreen()) {
+                z.page.trigger('navigate', urls.reverse('app', [slug]));
+            }
         }).fail(function() {
             forms.toggleSubmitFormState($this, true);
-            notify({message: gettext('Error while submitting report')});
+            notify({message: gettext('There was an issue submitting your abuse report. Please try again later.')});
         });
+    }));
+
+    z.body.on('click', '.abuse a', function(e) {
+        // When clicking on Report Abuse link on detail pagje.
+        //   - On mobile, navigate to report abuse page.
+        //   - On desktop, show report abuse modal.
+        var slug = $(this).closest('.detail').data('slug');
+
+        if (caps.widescreen()) {
+            e.preventDefault();
+            e.stopPropagation();
+            if (!$('mkt-prompt.abuse').length) {
+                z.body.append(nunjucks.env.render('_includes/abuse_modal.html', {
+                    slug: slug
+                }));
+            }
+        } else {
+            z.page.trigger('navigate', [urls.reverse('app/abuse', [slug])]);
+        }
     });
 
     return function(builder, args) {
-        builder.start('detail/abuse.html', {slug: args[0]}).done(function() {
-            $('.report-abuse').removeClass('modal');
-        });
-
         builder.z('type', 'leaf');
         builder.z('parent', urls.reverse('app', [args[0]]));
-        // L10n: Report abuse regarding an app
+        // L10n: Report abuse regarding an app.
         builder.z('title', gettext('Report Abuse'));
+
+        builder.start('app/abuse.html', {
+            slug: args[0]
+        });
     };
 });
