baface0695694263f1fa567c82de559c2195b24f
Kevin Ngo <ngoke@mozilla.com>
2014-12-22 18:20:00 -0800

    refresh review modals (bug 1114845)

--- src/media/js/views/app/ratings/add.js
+++ src/media/js/views/app/ratings/add.js
@@ -1,7 +1,6 @@
 define('views/app/ratings/add',
     ['capabilities', 'login', 'l10n', 'urls', 'user', 'z'],
     function(caps, login, l10n, urls, user, z) {
-
     var gettext = l10n.gettext;
 
     z.page.on('click', '.add-review-form .cancel', function(e) {
@@ -10,14 +9,13 @@ define('views/app/ratings/add',
         var app_detail_url = urls.reverse('app', [slug]);
 
         if (window.location.pathname !== app_detail_url) {
-            // On mobile, when we open a review we end up on a different page,
-            // so in order to cancel we need to go back to the app detail page.
-            // It'd cleaner to use navigation.back() but the parent is
-            // automatically pushed to the stack, so we'd end up in the app
-            // reviews list page even though we didn't come from there.
+            // When cancelling, we want to go back to the app detail page.
+            // navigation.back() would incorrectly to go to reviews list page.
             z.page.trigger('navigate', app_detail_url);
         }
-    }).on('click touchend', '.compose-review .rating', function() {
+    })
+
+    .on('click touchend', '.compose-review .rating', function() {
         // Scroll the page down to make the send/cancel buttons visible.
         var textarea = document.querySelector('.compose-review textarea:invalid');
         if (textarea) {
@@ -33,18 +31,16 @@ define('views/app/ratings/add',
     return function(builder, args) {
         var slug = args[0];
 
+        builder.z('type', 'leaf');
+        builder.z('parent', urls.reverse('app/ratings', [slug]));
+        builder.z('title', gettext('Leave a Review'));
+
         // If the user isn't logged in, redirect them to the detail page.
         if (!user.logged_in()) {
             z.page.trigger('navigate', urls.reverse('app', [slug]));
             return;
         }
 
-        builder.start('ratings/write.html', {'slug': slug}).done(function() {
-            $('.compose-review').removeClass('modal');
-        });
-
-        builder.z('type', 'leaf');
-        builder.z('parent', urls.reverse('app/ratings', [slug]));
-        builder.z('title', gettext('Write a Review'));
+        builder.start('ratings/add.html', {'slug': slug});
     };
 });
