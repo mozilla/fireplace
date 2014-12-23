de4426c0dc8881ff2fe1ea55fa8200a1d9eb75cf
Kevin Ngo <ngoke@mozilla.com>
2014-12-22 21:07:40 -0800

    add edit review modal

--- src/media/js/views/app/ratings/add.js
+++ src/media/js/views/app/ratings/add.js
@@ -3,26 +3,14 @@ define('views/app/ratings/add',
     function(caps, login, l10n, urls, user, z) {
     var gettext = l10n.gettext;
 
-    z.page.on('click', '.add-review-form .cancel', function(e) {
-        e.preventDefault();
-        var slug = $(this).closest('.add-review-form').attr('data-app');
-        var app_detail_url = urls.reverse('app', [slug]);
-
-        if (window.location.pathname !== app_detail_url) {
-            // When cancelling, we want to go back to the app detail page.
-            // navigation.back() would incorrectly to go to reviews list page.
-            z.page.trigger('navigate', app_detail_url);
-        }
-    })
-
-    .on('click touchend', '.compose-review .rating', function() {
+    z.page.on('click touchend', '.add-review .rating', function() {
         // Scroll the page down to make the send/cancel buttons visible.
-        var textarea = document.querySelector('.compose-review textarea:invalid');
+        var textarea = document.querySelector('.add-review textarea:invalid');
         if (textarea) {
             textarea.focus();
         }
 
-    }).on('focus', '.compose-review textarea', function() {
+    }).on('focus', '.add-review textarea', function() {
         if (window.scrollTo && !caps.widescreen()) {
             window.scrollTo(0, 200);
         }
@@ -30,9 +18,7 @@ define('views/app/ratings/add',
 
     return function(builder, args) {
         var slug = args[0];
-
         builder.z('type', 'leaf');
-        builder.z('parent', urls.reverse('app/ratings', [slug]));
         builder.z('title', gettext('Leave a Review'));
 
         // If the user isn't logged in, redirect them to the detail page.
