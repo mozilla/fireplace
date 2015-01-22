d65cb87bb054f8661e82bc616c15921c7aecca6f
Kevin Ngo <ngoke@mozilla.com>
2015-01-22 15:49:12 -0800

    refresh scroll tests

--- tests/ui/scroll.js
+++ tests/ui/scroll.js
@@ -1,37 +1,43 @@
+/*
+    Test that navigating to a page and back maintains scroll position.
+*/
+var appList = require('../lib/app_list');
 var helpers = require('../lib/helpers');
 
-var firstItemSel = '#gallery .listing li:first-child a';
-var scrollPos;
+var appSelector = appList.appNthChild(1) + ' .mkt-tile';
 
-helpers.startCasper({path: '/category/games'});
-
-casper.test.begin('General Scroll tests', {
-
+casper.test.begin('Scroll tests', {
     test: function(test) {
+        helpers.startCasper({path: '/category/games'});
 
-        casper.waitForSelector('#gallery .listing', function() {
+        // Open app list, scroll down, click on app.
+        casper.waitForSelector('.app-list', function() {
             scrollPos = casper.evaluate(function(Y) {
                 window.scrollTo(0, Y);
                 return window.scrollY;
-            }, casper.getElementBounds(firstItemSel).top);
-            casper.click(firstItemSel);
+            }, casper.getElementBounds(appSelector).top);
+            casper.click(appSelector);
         });
 
-        casper.wait(300, function() {
+        // Test it automatically scrolled to top, then go back.
+        casper.waitForSelector('[data-page-type~=detail]', function() {
+            casper.wait(300, function() {  // Wait for scroll to kick in.
+                var scrollY = casper.evaluate(function() {
+                    return window.scrollY;
+                });
+                test.assertEquals(scrollY, 0, 'Check scroll is 0');
+                casper.back();
+            });
+        });
+
+        // Check that the scroll is reverted back.
+        casper.waitForSelector('.app-list', function() {
             var scrollY = casper.evaluate(function() {
                 return window.scrollY;
             });
-            test.assertEquals(scrollY, 0, 'Check scroll is 0');
-            casper.back();
-        });
-
-        casper.wait(300, function() {
-            var scrollY = casper.evaluate(function() {
-                return window.scrollY;
-            });
-            test.assert(scrollY > 0, 'Check scroll is greater than 0');
-            test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
-            casper.click(firstItemSel);
+            test.assert(scrollY > 0, 'Check scroll greater than 0');
+            test.assertEquals(scrollY, scrollPos, 'Check scroll not changed');
+            casper.click(appSelector);
         });
 
         casper.waitForSelector('#nav-back', function() {
@@ -46,8 +52,6 @@ casper.test.begin('General Scroll tests', {
             test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
         });
 
-        casper.run(function() {
-            test.done();
-        });
+        helpers.done(test);
     }
 });
