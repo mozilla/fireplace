cd94b3697a84e0d32f41892e7cc0e4ff0e14f178
Andy McKay <amckay@mozilla.com>
2015-02-16 10:39:47 -0800

    feedback and l10n (bug 1129065)

--- src/media/js/views/usage.js
+++ src/media/js/views/usage.js
@@ -1,14 +1,13 @@
 define('views/usage',
     ['l10n'],
     function(l10n) {
-    'use strict';
+        'use strict';
+        var gettext = l10n.gettext;
 
-    var gettext = l10n.gettext;
-
-    return function(builder, args, params) {
-        builder.start('usage.html');
-        builder.z('type', 'leaf');
-        builder.z('title', gettext('App Statistics'));
-    };
-});
-
+        return function(builder, args, params) {
+            builder.z('type', 'leaf');
+            builder.z('title', gettext('App Statistics'));
+            builder.start('usage.html');
+        };
+    }
+);
