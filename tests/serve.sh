a4a4c22a9f9f4c19e5b71cef82cd4d83bb12f804
Mark Striemer <mstriemer@mozilla.com>
2015-03-26 16:20:11 -0500

    Check that flue is running in tests/serve.sh

--- tests/serve.sh
+++ tests/serve.sh
@@ -10,3 +10,6 @@ mv src/media/js/settings_local_test.js src/media/js/settings_local.js
 make build
 MKT_COMPILED=1 make serve &
 sleep 10
+
+# Make sure flue is ready.
+curl http://localhost:5000/api/v2/services/config/site/
