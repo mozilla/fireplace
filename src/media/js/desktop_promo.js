c49fcd23f354206fbafaa3794af77bd9033e812a
Mark Striemer <mstriemer@mozilla.com>
2015-05-08 14:40:43 -0500

    Provide promo item name in an l10n comment for desktop promo (bug 1108612)

--- src/media/js/desktop_promo.js
+++ src/media/js/desktop_promo.js
@@ -18,29 +18,33 @@ define('desktop_promo',
                 url: urls.reverse(
                     'feed_landing',
                     ['collection', 'puzzle-gamesdesktop-essentials']),
+                // L10n: Puzzle Games collection desktop promo (bug 1160327).
                 text: gettext('Puzzle Games—Desktop Essentials'),
             },
             {
                 name: 'littlealchemy',
                 url: urls.reverse('app', ['littlealchemy']),
+                // L10n: Little Alchemy app desktop promo (bug 1160327).
                 text: gettext('Play the addicting game of elements.'),
             },
             {
                 name: 'cut-the-rope',
                 url: urls.reverse('app', ['cut-the-rope']),
+                // L10n: Cut the Rope app desktop promo (bug 1155001).
                 text: gettext('Om Nom needs candy!'),
-
             },
             {
                 name: 'productivity',
                 url: urls.reverse('feed_landing', ['collection',
                                                    'productivity-appsdesktop']),
+                // L10n: Productivity collection desktop promo (bug 1108612).
                 text: gettext('Productivity Apps—Desktop Essentials'),
             },
             {
                 name: 'games',
                 url: urls.reverse('feed_landing', ['collection',
                                                    'games-ent-appsdesktop']),
+                // L10n: Games & Entertainment collection desktop promo (bug 1108612).
                 text: gettext('Games & Entertainment Apps—Desktop Essentials'),
             },
         ].map(function(item) {
