548bda0b7bc6d64c377c303c0c993a18f0c8db70
Kevin Ngo <ngoke@mozilla.com>
2015-05-15 16:12:54 -0700

    hook up the doc_type filter

--- src/media/js/route_api_args.js
+++ src/media/js/route_api_args.js
@@ -1,9 +1,16 @@
 define('route_api_args',
-    ['compat_filter', 'core/router', 'user_helpers'],
-    function(compatFilter, router, userHelpers) {
+    ['compat_filter', 'content_filter', 'core/router', 'core/settings',
+     'user_helpers', 'underscore'],
+    function(compatFilter, contentFilter, router, settings,
+             userHelpers, _) {
 
     router.api.addProcessor(function(endpoint) {
         var args = compatFilter.apiArgs(endpoint);
+
+        if (settings.meowEnabled) {
+            args = _.extend(args, contentFilter.apiArgs(endpoint));
+        }
+
         args.region = userHelpers.region(undefined, true);
         args.carrier = userHelpers.carrier();
         return args;
