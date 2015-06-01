1365534a2b5fd696a97540973ae2bc32f9f20a58
Rob Hudson <robhudson@mozilla.com>
2015-06-01 12:24:49 -0700

    Update categories for HOW Phase 0 (bug 1161869)

--- src/media/js/categories.js
+++ src/media/js/categories.js
@@ -4,22 +4,29 @@ define('categories', ['core/l10n'], function(l10n) {
 
     return [
         {slug: 'games', name: gettext('Games')},
-        {slug: 'books', name: gettext('Books')},
+        {slug: 'books-comics', name: gettext('Books & Comics')},
         {slug: 'business', name: gettext('Business')},
         {slug: 'education', name: gettext('Education')},
         {slug: 'entertainment', name: gettext('Entertainment')},
+        {slug: 'food-drink', name: gettext('Food & Drink')},
         {slug: 'health-fitness', name: gettext('Health & Fitness')},
+        {slug: 'humor', name: gettext('Humor')},
+        {slug: 'internet', name: gettext('Internet')},
+        {slug: 'kids', name: gettext('Kids')},
         {slug: 'lifestyle', name: gettext('Lifestyle')},
         {slug: 'maps-navigation', name: gettext('Maps & Navigation')},
         {slug: 'music', name: gettext('Music')},
-        {slug: 'news-weather', name: gettext('News & Weather')},
+        {slug: 'news', name: gettext('News')},
+        {slug: 'personalization', name: gettext('Personalization')},
         {slug: 'photo-video', name: gettext('Photo & Video')},
         {slug: 'productivity', name: gettext('Productivity')},
         {slug: 'reference', name: gettext('Reference')},
+        {slug: 'science-tech', name: gettext('Science & Tech')},
         {slug: 'shopping', name: gettext('Shopping')},
         {slug: 'social', name: gettext('Social')},
         {slug: 'sports', name: gettext('Sports')},
         {slug: 'travel', name: gettext('Travel')},
-        {slug: 'utilities', name: gettext('Utilities')}
+        {slug: 'utilities', name: gettext('Utilities')},
+        {slug: 'weather', name: gettext('Weather')}
     ];
 });
