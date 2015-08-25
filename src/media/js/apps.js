134df23e19ba2f56acd1be8589095126a8d692e5
Kevin Ngo <me@ngokevin.com>
2015-08-25 11:17:09 -0700

    don't disable game websites on mobile (bug 1195969)

--- src/media/js/apps.js
+++ src/media/js/apps.js
@@ -144,14 +144,6 @@ define('apps',
             app.previews = [];
             app.contentType = 'website';
             app.key = app.id;
-
-            if (settings.gametimeEnabled &&
-                app.isWebsite &&
-                app.device_types.indexOf(capabilities.os.type) === -1 &&
-                app.keywords.indexOf('featured-game') !== -1) {
-              // Don't show desktop website games on mobile.
-              app.disabled = true;
-            }
         } else {
             app.isApp = true;
             app.contentType = 'app';
