fd4d28e82cc0edd7381195c97afc95f083ac8d68
Chuck Harmston <chuck@chuckharmston.com>
2015-08-27 14:46:28 -0600

    Adds add-on listing page, personalization submenu.

--- src/media/js/views/category.js
+++ src/media/js/views/category.js
@@ -39,6 +39,8 @@ define('views/category',
                 sort: 'reviewed',
                 src: newSrc
             }),
+            showPersonalizationSubmenu: (slug === 'personalization' &&
+                                         settings.addonsEnabled),
             sort: params.sort,
             source: params.sort ? newSrc: popularSrc,
         });
