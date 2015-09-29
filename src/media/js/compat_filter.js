mozilla/fireplace

commit ed1670215596a33c5744f3065ebdacc702f17329

remove platform selector for mobile (bug 1205141)

src/media/js/compat_filter.js
@@ -49,25 +49,20 @@ define('compat_filter',
 
     // Calculate device filter choices.
     var DEVICE_FILTER_CHOICES = [
-        ['all', gettext('All Platforms')]
+        ['all', gettext('All Platforms')],
+        ['desktop', gettext('Desktop')],
+        ['firefoxos', gettext('Firefox OS')],
+        ['android-mobile', gettext('Android Mobile')],
+        ['android-tablet', gettext('Android Tablet')]
     ];
-    if (caps.firefoxOS || caps.firefoxAndroid) {
-        DEVICE_FILTER_CHOICES.splice(1, 0, [caps.device_type(),
-                                            gettext('My Device')]);
-    } else {
-        DEVICE_FILTER_CHOICES = DEVICE_FILTER_CHOICES.concat([
-            ['desktop', gettext('Desktop')],
-            ['firefoxos', gettext('Firefox OS')],
-            ['android-mobile', gettext('Android Mobile')],
-            ['android-tablet', gettext('Android Tablet')]
-        ]);
-    }
 
     var actualPlatform = caps.device_platform();
     var actualFormFactor = caps.device_formfactor();
     var filterDeviceLSKey = 'filter-device';
     var filterDeviceFromLS = storage.getItem(filterDeviceLSKey);
     var filterDevice = filterDeviceFromLS || caps.device_type();
+
+    z.body.attr('data-platform', actualPlatform);
 
     z.body.on('change', '.compat-filter', function() {
         // Update device preferences and reload view to refresh changes.
@@ -148,7 +143,6 @@ define('compat_filter',
     });
 
     return {
-        DEVICE_CHOICES: DEVICE_CHOICES,
         DEVICE_FILTER_CHOICES: DEVICE_FILTER_CHOICES,
         apiArgs: apiArgs,
         featureProfile: featureProfile,

tests/unit/compat_filter.js
@@ -84,16 +84,6 @@ define('tests/unit/compat_filter',
                 assert.equal(compatFilter.getFilterDevice(), 'android-mobile');
             }));
 
-        it('has expected options for android-mobile',
-            helpers
-            .injector(androidCapabilities('mobile'))
-            .run(['compat_filter'], function(compatFilter) {
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES.length, 2);
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[0][0], 'all');
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[1][0], 'android-mobile');
-            }));
-
-
         it('set device android tablet',
             helpers
             .injector(androidCapabilities('tablet'))
@@ -108,15 +98,6 @@ define('tests/unit/compat_filter',
                 assert.equal(compatFilter.getFilterDevice(), 'android-tablet');
             }));
 
-        it('has expected options for android-tablet',
-            helpers
-            .injector(androidCapabilities('tablet'))
-            .run(['compat_filter'], function(compatFilter) {
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES.length, 2);
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[0][0], 'all');
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[1][0], 'android-tablet');
-            }));
-
         it('set device to firefoxos',
             helpers
             .injector(firefoxOSCapabilities, noStorage)
@@ -129,15 +110,6 @@ define('tests/unit/compat_filter',
                 });
                 assert.equal(compatFilter.isDeviceSelected('firefoxos'), true);
                 assert.equal(compatFilter.getFilterDevice(), 'firefoxos');
-            }));
-
-        it('has expected options for firefoxos',
-            helpers
-            .injector(firefoxOSCapabilities, noStorage)
-            .run(['compat_filter'], function(compatFilter) {
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES.length, 2);
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[0][0], 'all');
-                assert.equal(compatFilter.DEVICE_FILTER_CHOICES[1][0], 'firefoxos');
             }));
 
         it('set feature profiles',
@@ -158,22 +130,6 @@ define('tests/unit/compat_filter',
 
                 // No profile if endpoint not supported.
                 assert.equal(compatFilter.apiArgs('').pro, undefined);
-            }));
-
-        it('sets initialDeviceText',
-            helpers
-            .injector(firefoxOSCapabilities, noStorage)
-            .run(['compat_filter'], function(compatFilter) {
-                assert.equal(compatFilter.initialDeviceText,
-                             'My Device');
-            }));
-
-        it('sets initialDeviceText for Android',
-            helpers
-            .injector(androidCapabilities('tablet'), noStorage)
-            .run(['compat_filter'], function(compatFilter) {
-                assert.equal(compatFilter.initialDeviceText,
-                             'My Device');
             }));
     });
 });

src/media/css/compat-filter.styl
@@ -1,13 +1,16 @@
 @import "lib";
 
-[data-page-type~="app-list"],
-[data-page-type~="collection-landing"],
-[data-page-type~="editorial-landing"],
-[data-page-type~="shelf-landing"],
-[data-page-type~="homepage"] {
-    .compat-filter {
-        display: block;
-    }
+
+body[data-platform="desktop"] {
+  &[data-page-type~="app-list"],
+  &[data-page-type~="collection-landing"],
+  &[data-page-type~="editorial-landing"],
+  &[data-page-type~="shelf-landing"],
+  &[data-page-type~="homepage"] {
+      .compat-filter {
+          display: block;
+      }
+  }
 }
 
 .compat-filter {

commit 5f97d0cc20f6bf946c64fbabb50521a2a4272388

Fix the compatibility text (bug 1208862)
This fixes a regression from ed1670215596a33c5744f3065ebdacc702f17329.

src/media/js/compat_filter.js
@@ -143,6 +143,7 @@ define('compat_filter',
     });
 
     return {
+        DEVICE_CHOICES: DEVICE_CHOICES,
         DEVICE_FILTER_CHOICES: DEVICE_FILTER_CHOICES,
         apiArgs: apiArgs,
         featureProfile: featureProfile,