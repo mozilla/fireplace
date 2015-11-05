00d4f23da6ac911f28f6276bcecfb92fee2e973e
Davor Spasovski <davor@spasovski.net>
2015-11-05 15:06:09 -0500

    js inject overlay close btn (bug 1220768)

--- src/media/js/nav.js
+++ src/media/js/nav.js
@@ -67,8 +67,8 @@ define('nav', ['core/log', 'core/navigation', 'core/views', 'core/z'],
     }
 
     // Main mobile nav.
-    z.body.on('click touchend', '.global-nav-menu a', function(e) {
-        var $this = $(e.target);
+    z.body.on('click', '.global-nav-menu', function(e) {
+        var $this = $(e.target).closest('.global-nav-link');
         setMenuState($this.data('nav-type'));
 
         if ($this.data('nav-type') === 'more') {
@@ -80,11 +80,18 @@ define('nav', ['core/log', 'core/navigation', 'core/views', 'core/z'],
         z.body.trigger('showoverlay', {selector: '.cat-menu-overlay'});
     }).on('showoverlay', function(e, overlay) {
         $(overlay.selector).addClass('overlay-visible');
-        $('.overlay-close').addClass('overlay-close-visible');
         z.page.trigger('clearsearch');
+        z.body.addClass('overlayed');
+
+        // Inject overlay clear button.
+        setTimeout(function() {
+            z.body.append('<button class="overlay-close"></button>');
+            $('.overlay-close').addClass('overlay-close-visible');
+        }, 200);
     }).on('hideoverlay', function() {
+        $('.overlay-close').remove();
+        z.body.removeClass('overlayed');
         $('.full-screen-overlay').removeClass('overlay-visible');
-        $('.overlay-close').removeClass('overlay-close-visible');
         $('.nav-category-link.header-nav-link-active').removeClass('header-nav-link-active');
     })
     // Close mobile overlay button.
