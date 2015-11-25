c9477cfde67cfd9e745fcb5a834e44ba1e42fe04
Mathieu Pillard <m@virgule.net>
2015-11-25 16:36:50 +0100

    Remove startup_errors as the package already do those checks, and better (bug 1177847)

--- src/media/js/utils_local.js
+++ src/media/js/utils_local.js
@@ -28,93 +28,6 @@ define('utils_local',
         return items;
     };
 
-    function offline(def, socket) {
-        if (z.onLine) {
-            // Fire event for going offline.
-            logger.log('Offline detected.');
-            z.win.trigger('offline_detected');
-            z.onLine = false;
-        }
-        if (socket) {
-            reset_socket(socket);
-        }
-        def.reject();
-    }
-
-    function online(def, socket) {
-        if (!z.onLine) {
-            // Fire event for going online.
-            // Fire event to start loading images.
-            logger.log('Online detected.');
-            z.win.trigger('online_detected');
-            z.onLine = true;
-        }
-        if (socket) {
-            reset_socket(socket);
-        }
-        def.resolve();
-    }
-
-    function reset_socket(socket) {
-        socket.onopen = null;
-        socket.onerror = null;
-        socket.close();
-    }
-
-    function checkOnline() {
-        // `navigator.onLine` is unreliable (see bugs 654579 and 756364)
-        // so we need to test an actual connection.
-        var def = defer.Deferred();
-        if (window.__mockOffLine === true) {
-            offline(def);
-            return def.promise();
-        } else if (settings.allow_offline) {
-            online(def);
-            return def.promise();
-        }
-        try {
-            if (navigator.mozTCPSocket === null) {
-                return checkOnlineDesktop();
-            }
-            logger.log('Checking online state with socket');
-            var host = (new URL(urls.cdn_url())).host;
-            var port = 80;
-            var socket = navigator.mozTCPSocket.open(host, port);
-            socket.onerror = function(e) {
-                offline(def, socket);
-            };
-            socket.onopen = function(e) {
-                online(def, socket);
-            };
-        } catch (e) {
-            return checkOnlineDesktop();
-        }
-
-        return def.promise();
-    }
-
-    function checkOnlineDesktop() {
-        var def = defer.Deferred();
-
-        var i = new Image();
-        i.src = 'https://marketplace.cdn.mozilla.net/media/fireplace/img/online-status-beacon.gif?' + (+new Date());
-        i.onload = function() {
-            online(def);
-        };
-        i.onerror = function(e) {
-            offline(def);
-        };
-
-        return def.promise();
-    }
-
-    function pollOnlineState() {
-        if (check_interval) {
-            clearInterval(check_interval);
-        }
-        check_interval = setInterval(checkOnline, 10000);
-    }
-
     function initSalvattore(elem) {
         // Initializes Salvattore layout on an element.
         if (elem) {
@@ -139,11 +52,9 @@ define('utils_local',
 
     return {
         build_localized_field: build_localized_field,
-        checkOnline: checkOnline,
         headerTitle: headerTitle,
         initSalvattore: initSalvattore,
         items: items,
-        pollOnlineState: pollOnlineState,
     };
 
 });
