d76a5a3e53ee754dd5aa10ccbd86d8ca7d9e307b
Davor Spasovski <davor@spasovski.net>
2016-02-08 13:27:23 -0500

    add app name to its privacy policy page (bug 1189412)

--- src/media/js/views/app/privacy.js
+++ src/media/js/views/app/privacy.js
@@ -1,16 +1,20 @@
 define('views/app/privacy',
-    ['core/l10n', 'core/urls', 'utils_local'],
-    function(l10n, urls, utilsLocal) {
+    ['core/l10n', 'core/requests', 'core/urls', 'utils_local'],
+    function(l10n, requests, urls, utilsLocal) {
     'use strict';
     var gettext = l10n.gettext;
 
     return function(builder, args) {
+        var slug = args[0];
         builder.z('type', 'leaf');
-        builder.z('parent', urls.reverse('app', [args[0]]));
-        var title = gettext('Privacy Policy');
-        builder.z('title', title);
-        utilsLocal.headerTitle(title);
+        builder.z('parent', urls.reverse('app', [slug]));
 
-        builder.start('app/privacy.html', {slug: args[0]});
+        requests.get(urls.api.url('app', [slug])).done(function(app) {
+            var title = app.name;
+            builder.z('title', title);
+            utilsLocal.headerTitle(title);
+        });
+
+        builder.start('app/privacy.html', {slug: slug});
     };
 });
