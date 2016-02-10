fecdc68691c7699e147fa941989c066d4a040bce
Davor Spasovski <davor@spasovski.net>
2016-02-10 14:29:43 -0500

    consistent header for 'my apps' page (bug 1189322)

--- src/media/js/views/purchases.js
+++ src/media/js/views/purchases.js
@@ -1,14 +1,16 @@
 /*
     My Apps page.
 */
-define('views/purchases', ['core/l10n', 'core/urls'], function(l10n, urls) {
+define('views/purchases', ['core/l10n', 'utils_local'], function(l10n, utilsLocal) {
     'use strict';
     var gettext = l10n.gettext;
 
     return function(builder, args) {
-        builder.z('type', 'root settings sub_setting purchases');
-        builder.z('title', gettext('My Apps'));
-        builder.z('parent', urls.reverse('homepage'));
+        var title = gettext('My Apps');
+
+        builder.z('type', 'leaf spoke-header settings sub_setting purchases');
+        builder.z('title', title);
+        utilsLocal.headerTitle(title);
 
         builder.start('purchases.html', {
             endpoint_name: 'installed'
